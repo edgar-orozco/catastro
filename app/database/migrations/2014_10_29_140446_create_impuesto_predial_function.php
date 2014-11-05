fa1b9ac2782db58795b8a8f395bbe0b76512378d
Edgar <edgar.orozco.juarez@gmail.com>
2014-11-04 18:37:03 -0600

    Se eliminan mensajes de debug y se pasa a % la tasa fiscal

--- app/database/migrations/2014_10_29_140446_create_impuesto_predial_function.php
+++ app/database/migrations/2014_10_29_140446_create_impuesto_predial_function.php
@@ -13,19 +13,19 @@ class CreateImpuestoPredialFunction extends Migration {
     {
         $sql = <<<FinFunc
     --Función para calcular el impuesto predial.
-    CREATE OR REPLACE FUNCTION impuesto_predial(tipo_predio VARCHAR, valor_catastral DOUBLE PRECISION, anioinp INT, mesinp INT)
-     RETURNS DOUBLE PRECISION AS $$
+    CREATE OR REPLACE FUNCTION impuesto_predial(tipo_predio VARCHAR, valor_catastral NUMERIC, anioinp INT, mesinp INT)
+     RETURNS NUMERIC AS $$
     DECLARE
-        cf DOUBLE PRECISION; -- La cuota fija. TODO: Se debe pasar a NUMERIC
-        ex DOUBLE PRECISION; -- El porcentaje sobre excedente. TODO: Se debe pasar a NUMERIC
-        li DOUBLE PRECISION; -- El límite inferior. TODO: Se debe pasar a NUMERIC
-        ls DOUBLE PRECISION; -- El límite superior. TODO: Se debe pasar a NUMERIC
-        vf DOUBLE PRECISION; -- El valor fiscal. (valor_catastral * .20) TODO: Se debe pasar a NUMERIC
-        smv DOUBLE PRECISION; -- El salario mínimo vigente. TODO: Se debe pasar a NUMERIC
-        exc DOUBLE PRECISION; -- El valor excedente (valor fiscal - límite inferior). TODO: Se debe pasar a NUMERIC
-        impMinUrbano DOUBLE PRECISION; --El impuesto minimo anual para urbano. TODO: Se debe pasar a NUMERIC
-        impMinRustico DOUBLE PRECISION; --El impuesto minimo anual para rustico . TODO: Se debe pasar a NUMERIC
-        impuesto DOUBLE PRECISION; --El impuesto predial resultante. TODO: Se debe pasar a NUMERIC
+        cf NUMERIC; -- La cuota fija.
+        ex NUMERIC; -- El porcentaje sobre excedente.
+        li NUMERIC; -- El límite inferior.
+        ls NUMERIC; -- El límite superior.
+        vf NUMERIC; -- El valor fiscal. (valor_catastral * .20)
+        smv NUMERIC; -- El salario mínimo vigente.
+        exc NUMERIC; -- El valor excedente (valor fiscal - límite inferior).
+        impMinUrbano NUMERIC; --El impuesto minimo anual para urbano.
+        impMinRustico NUMERIC; --El impuesto minimo anual para rustico.
+        impuesto NUMERIC; --El impuesto predial resultante.
     BEGIN
 
         smv := 63.77; --TODO: Se debe sacar de una tabla de configuracion de smv
@@ -37,8 +37,10 @@ class CreateImpuestoPredialFunction extends Migration {
 
         impMinRustico := 3 * smv; --TODO: Se debe sacar de una tabla de impuesto minimo anual (sale de la ley art 97)
         impMinUrbano := 4 * smv;
+
         exc = vf - li;
-        impuesto := (vf - li) * ex + cf;
+
+        impuesto := (vf - li) * ex / 100 + cf;
 
         IF tipo_predio = 'U' AND impuesto < impMinUrbano THEN impuesto := impMinUrbano; END IF;
         IF tipo_predio = 'R' AND impuesto < impMinRustico THEN impuesto := impMinRustico; END IF;
@@ -47,7 +49,7 @@ class CreateImpuestoPredialFunction extends Migration {
         --RAISE NOTICE 'Min U: %',impMinUrbano;
         --RAISE NOTICE 'Min R: %',impMinRustico;
         --RAISE NOTICE 'cf: %,excd: % psex: % li: %, ls: %, vf: % ', cf, exc, ex, li, ls, vf;
-        --RAISE NOTICE ' => (valor_catastral * 0.2 - li) * ex + cf => (% - %) * % + % = %', vf, li, ex, cf, impuesto;
+        --RAISE NOTICE ' => (valor_catastral * 0.2 - li) * ex / 100 + cf => (% - %) * % / 100 + % = %', vf, li, ex, cf, impuesto;
         RETURN impuesto;
     END;
     $$ LANGUAGE plpgsql;
@@ -61,7 +63,7 @@ FinFunc;
 	 */
 	public function down()
 	{
-        $sql = "DROP FUNCTION IF EXISTS impuesto_predial(varchar, double precision, int, int);";
+        $sql = "DROP FUNCTION IF EXISTS impuesto_predial(varchar, NUMERIC, int, int);";
         DB::connection()->getPdo()->exec($sql);
 	}
 
