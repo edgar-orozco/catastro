60efd71e3a0e866e6039fd436288f018ac30bfd5
Edgar <edgar.orozco.juarez@gmail.com>
2014-11-27 22:20:15 -0600

    Se agrega estilo temporal para centrar el panel de login

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -1,48 +1,52 @@
 @extends('layouts.default')
 
+@section('styles')
+<style>
+    .login {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        width: 350px;
+        height: 200px;
+        margin-left: -150px;
+        margin-top: -150px;
+    }
+</style>
+@stop
+
 @section('content')
 
-<div class="container">
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+<div class="row">
+    <div class="login">
+        <div class="panel">
+            <div class="panel-heading">
+                <h3 class="panel-title">Sistema de Gestión Catastral</h3>
+            </div>
+            <div class="panel-body">
+                {{ Form::open(array('url' => 'users/login', 'method' => 'POST')) }}
 
-    <div class="row">
-        <div class="col-md-4 col-md-offset-4">
-            <div class="panel">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Sistema de Gestión Catastral</h3>
-                </div>
-                <div class="panel-body">
-                    {{ Form::open(array('url' => 'users/login', 'method' => 'POST')) }}
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <span class="glyphicon glyphicon-user"></span>
+                        </span>
+                        {{ Form::text('username', null, array('class' => 'form-control focus', 'placeholder'=>'Nombre de usuario', 'autofocus'=> 'autofocus')) }}
+                    </div>
 
-                        <div class="input-group">
-                            <span class="input-group-addon">
-                                <span class="glyphicon glyphicon-user"></span>
-                            </span>
-                            {{ Form::text('username', null, array('class' => 'form-control focus', 'placeholder'=>'Nombre de usuario', 'autofocus'=> 'autofocus')) }}
-                        </div>
+                    <br/>
 
-                        <br/>
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <span class="glyphicon glyphicon-lock"></span>
+                        </span>
+                        {{ Form::password('password', array('class' => 'form-control', 'placeholder'=>'Contraseña')) }}
+                    </div>
 
-                        <div class="input-group">
-                            <span class="input-group-addon">
-                                <span class="glyphicon glyphicon-lock"></span>
-                            </span>
-                            {{ Form::password('password', array('class' => 'form-control', 'placeholder'=>'Contraseña')) }}
-                        </div>
+                    <br/>
 
-                        <br/>
-
-                        {{ Form::submit('Entrar al sistema', array('class' => 'btn btn-primary')) }}
-                    {{ Form::close() }}
-                </div>
+                    {{ Form::submit('Entrar al sistema', array('class' => 'btn btn-primary')) }}
+                {{ Form::close() }}
             </div>
         </div>
     </div>
 </div>
-
 @stop
