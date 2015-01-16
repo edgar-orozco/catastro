d1b9c73e0851e3e7dd3a59224f3ab4b21abdafac
Edgar <edgar.orozco.juarez@gmail.com>
2015-01-16 01:08:34 -0600

    Se crean tablas de intentos y relación de usuarios y municipios

--- app/database/migrations/2015_01_15_212047_create_intentos_table.php
+++ app/database/migrations/2015_01_15_212047_create_intentos_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class AddIntentosTable extends Migration {
+class CreateIntentosTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -20,8 +20,8 @@ class AddIntentosTable extends Migration {
 			$table->boolean('noencontrado')->default(false);
 			$table->integer('tipotramite_id');
 			$table->integer('usuario_id');
-			$table->foreign('users')->references('id')->onUpdate('cascade')->onDelete('cascade');
-			$table->foreign('tipotramites')->references('id')->onUpdate('cascade')->onDelete('cascade');
+			$table->foreign('usuario_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+			$table->foreign('tipotramite_id')->references('id')->on('tipotramites')->onUpdate('cascade')->onDelete('cascade');
 			$table->timestamps();
 		});
 
@@ -29,10 +29,12 @@ class AddIntentosTable extends Migration {
 		{
 			$table->increments('id');
 			$table->integer('requisito_id');
+			$table->integer('intento_id');
+			$table->foreign('requisito_id')->references('id')->on('requisitos')->onUpdate('cascade')->onDelete('cascade');
+			$table->foreign('intento_id')->references('id')->on('intentos')->onUpdate('cascade')->onDelete('cascade');
 		});
 
 	}
-
 
 	/**
 	 * Reverse the migrations.
---
+++ app/database/migrations/2015_01_16_002013_create_municipios_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateMunicipiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('municipios')) {
+
+            Schema::create('municipios', function (Blueprint $table) {
+                $table->string('entidad', 2);
+                $table->string('municipio', 3);
+                $table->text('nom_mpo');
+                $table->text('nom_cabecera');
+                $table->primary('municipio');
+            });
+        }
+
+        if (!Schema::hasTable('user_municipio')) {
+
+            Schema::create('user_municipio', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('usuario_id');
+                $table->string('municipio_id', 3);
+                $table->foreign('usuario_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+                $table->foreign('municipio_id')->references('municipio')->on('municipios')->onUpdate('cascade')->onDelete('cascade');
+                $table->unique(['usuario_id', 'municipio_id']);
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('municipios');
+        Schema::drop('user_municipio');
+    }
+}
