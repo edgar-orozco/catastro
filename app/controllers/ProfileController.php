2c5240639b8733d4efe6ab5ca18f133607158860
David Galindo <david@Mac-mini-de-David.local>
2015-02-06 13:15:08 -0600

    Se crea el método en el control del profile para editar datos

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -18,4 +18,19 @@ class ProfileController extends BaseController
 
         return View::make('profile.index', compact('title', 'title_section'));
     }
+    /**
+     * Muestra la pantalla de consulta de profile.
+     * @return \Illuminate\View\View
+     */
+    public function edit()
+    {
+        // Title
+        $title = "Mis datos";
+        // Title
+        $title_section = "Mis datos";
+        // Subtitle
+        $subtitle_section = "Modificar datos";
+
+        return View::make('profile.edit', compact('title', 'title_section', 'subtitle_section'));
+    }
 }
