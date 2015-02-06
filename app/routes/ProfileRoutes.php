72a45b6d80fc99c639ce0370deda44cfd742cefe
David Galindo <david@Mac-mini-de-David.local>
2015-02-06 08:45:30 -0600

    Se crea la ruta para mostrar los datos del profile del usuario

---
+++ app/routes/ProfileRoutes.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by David.
+ */
+
+/**
+ * Rutas para el profile del usuario
+ */
+
+Route::get('profile/mis-datos', array(
+    'as' => 'profle.mis-datos',
+    'uses' => '',
+    'before' => 'auth'
+));
