c131f0527fec05bc04393ecc3c33a2838bbdd331
David Galindo <david@Mac-mini-de-David.local>
2015-02-06 16:02:26 -0600

    Se agrega enlace Carga Cartográfica al menú del administrador y superusuario

--- app/views/administrador.blade.php
+++ app/views/administrador.blade.php
@@ -110,5 +110,28 @@
             </div>
         </div>
 
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="glyphicon glyphicon-cloud-upload gi-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge">0</div>
+                            <div>Archivos cargados</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left">Carga Cartográfica</span>
+                        <span class="pull-right"><i class="glyphicon glyphicon-circle-arrow-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
     </div>
 @stop
