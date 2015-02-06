463c7dde9079bc14b850c1fe90850b71665d40ed
David Galindo <david@Mac-mini-de-David.local>
2015-02-06 08:48:52 -0600

    Se crea el control principal para el profile del usuario

---
+++ app/controllers/ProfileController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by David
+ */
+class ProfileController extends BaseController
+{
+
+    /**
+     * Muestra la pantalla de consulta de profile.
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+
+    }
+}
