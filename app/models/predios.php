35d5c0001d95db111dba5955b1accba93a436b93
vagrant <landyggonzalez@gmial.com>
2015-02-13 19:43:53 -0600

    se corije el modelo y se agrega el menu

--- app/models/predios.php
+++ app/models/predios.php
@@ -3,20 +3,8 @@
 class predios extends Eloquent
 {
     protected $table ='predios';
-<<<<<<< HEAD
     protected $primaryKey = 'clave';
     public $timestamps=false;
     protected $guarded = array("*");
    // protected $fillable = array("nombrec","rfc");
-
-   
-
 }
-=======
-    protected $primaryKey = 'gid';
-    public $timestamps=false;
-    protected $guarded = array("*");
-    protected $fillable = array("gid","id_propietario","municipio");
-}
-
->>>>>>> aaab2b194863aa20239b95ff595d18a053dadbf3
