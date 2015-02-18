4a2a3da69542e2f6db1a8a7b1f5c08cc53a4800a
System Administrator <root@MacBook-Pro-de-RABBOT-2.local>
2015-02-18 13:47:23 -0600

    cambios login geo

--- app/views/layouts/nosession.blade.php
+++ app/views/layouts/nosession.blade.php
@@ -68,6 +68,61 @@
 
     @yield('content')
 </div>
+<footer>
+    <div class="container">
+        <div class="col-md-4 col-lg-4 col-sm-4">
+            <h2>Gobierno de <b>Tabasco</b></h2>
+            <ul>
+                <li>
+                    <a href="http://104.236.22.240/users/login">Portal Transparencia</a>
+                </li>
+                <li>
+                    <a href="http://104.236.22.240/users/login">ITAIP</a>
+                </li>
+                <li>
+                    <a href="http://104.236.22.240/users/login">Infomex</a>
+                </li>
+                <li>
+                    <a href="http://104.236.22.240/users/login">Aviso de Privacidad</a>
+                </li>
+                <li>
+                    <a href="http://104.236.22.240/users/login">Buzón</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-4 col-lg-4 col-sm-4">
+            <h2>Dirección / <b>Ubicación</b></h2>
+            <p>
+                Independencia No. 2, Col. Centro Palacio, <br>
+                de Gobierno, C.P. 86000 Villahermosa, <br>
+                Tabasco, MX. <br>
+                Tel. (993) 358 0400
+            </p>
+        </div>
+        <div class="col-md-4 col-lg-4 col-sm-4">
+            <h2>Contactanos / <b>Comenta</b></h2>
+
+            <form action="">
+                <input type="text" placeholder="Nombre">
+                <input type="text" placeholder="Email">
+                <input type="text" placeholder="Teléfono">
+                <textarea name="" id="" cols="30" rows="10" placeholder="Comentarios"></textarea>
+                <input type="submit" value="Enviar">
+            </form>
+        </div>
+    </div>
+</footer>
+<div class="footer legal container">
+    <div class="col-sm-8 col-md-8 col-lg-8">
+        <p><b>Gobierno del Estado de Tabasco © Derechos Reservados 2013 - 2018</b><br>
+            Dirección General de Tecnologías de Información y Comunicaciones</p>
+    </div>
+    <div class="col-sm-4 col-md-4 col-lg-4">
+        <div class="img-cont">
+            <img src="http://104.236.22.240/css/images/main/main-logo.png" alt="Catastro">
+        </div>
+    </div>
+</div>
 
 <!-- JQuery -->
 {{ HTML::script('js/jquery/jquery.min.js') }}
