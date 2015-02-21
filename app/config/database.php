409f1c523fd22b8ec5ebbe0088950f892198995a
deltacastro <abel.castro9111@gmail.com>
2015-02-20 19:07:14 -0600

    Prueba

--- app/config/database.php
+++ app/config/database.php
@@ -66,7 +66,7 @@ return array(
 		'pgsql' => array(
 			'driver'   => 'pgsql',
 			'host'     => 'localhost',
-			'database' => 'Catastro',
+			'database' => 'catastro',
 			'username' => 'postgres',
 			'password' => 'pass',
 			'charset'  => 'utf8',
