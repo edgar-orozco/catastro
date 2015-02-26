b7a945ad8f1244a25900356db41f2a873f2d45d2
David Galindo <david@Mac-mini-de-David.local>
2015-02-25 23:54:36 -0600

    Se agrega helper para obtener el valor del parametro upload_max_filesize en un plantilla laravel

--- app/libraries/ShapesHelper.php
+++ app/libraries/ShapesHelper.php
@@ -5,8 +5,15 @@
  * Clase con helpers para la carga cartografica
  */
 
+/**
+ * Class ShapesHelper
+ * Clase con helper para la carga cartografica
+ */
 class ShapesHelper{
-
+    /**
+     * Funcion para contar el numero de archivos guardados en la ruta storage/shapes
+     * @return int|string
+     */
     public static function countShapes(){
         $dir = __DIR__ . '/../storage/shapes';
 
@@ -16,5 +23,31 @@ class ShapesHelper{
         $fi = new FilesystemIterator($dir, FilesystemIterator::SKIP_DOTS);
 
         return iterator_count($fi);
+    }
+
+    /**
+     * Funcion para obtener el tamaño maximo de archivos soportado por el servidor
+     * @return float
+     */
+    public static function serverUploadSize(){
+
+        return self::parseSize(ini_get('upload_max_filesize'));
+    }
+
+    /**
+     * Funcion para convertir el peso de un archivo en bytes
+     *
+     * @param $size
+     * @return float
+     */
+    private static function parseSize($size){
+        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+        $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+        if ($unit) {
+            // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
+            return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+        } else {
+            return round($size);
+        }
     }
 }
