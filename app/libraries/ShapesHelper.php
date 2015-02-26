8d30399e9109a8244c18ec8bacd179d13ff0a76a
David Galindo <david@Mac-mini-de-David.local>
2015-02-26 00:43:07 -0600

    Se funcion al helper Shapes para representar los bytes en forma "humana".

--- app/libraries/ShapesHelper.php
+++ app/libraries/ShapesHelper.php
@@ -35,6 +35,25 @@ class ShapesHelper{
     }
 
     /**
+     * Funcion para representar de forma mas humana los bytes
+     *
+     * @param $bytes
+     * @param int $precision
+     * @return string
+     */
+    public static function formatBytes($bytes, $precision = 2) {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        $bytes /= pow(1024, $pow);
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
+
+    /**
      * Funcion para convertir el peso de un archivo en bytes
      *
      * @param $size
