9f018777be0d615dae379cf32dde36062197f977
Roman <rmcentinela@gmail.com>
2015-02-27 10:16:00 -0600

    se carga seeder ejecutor

--- app/database/seeds/EjecutoresSeeder.php
+++ app/database/seeds/EjecutoresSeeder.php
@@ -1,35 +1,35 @@
 <?php
 
-class ejecutoresTableSeeder extends Seeder {
+class EjecutoresSeeder extends Seeder {
 
 	public function run()
 	{
-		$tt = ejecutores::create([
+	//	$tt = ejecutores::create([
 		
+	    DB::table('ejecutores')->insert(
+            array(  'id_ejecutor' => 1,   
+					'id_p'  => 18697,
+					'cargo'  => 'SUBDIRECTOR DE EJECUCIÓN FISCAL',
+					'titulo'  =>  'LIC',
+					'f_nombramiento'  => '2015-02-17',
+					'id_p_otorga_nombramiento'  => 18045,
+					'usuario'  => 1,
+					'f_alta'  => date('Y-m-d H:i:s'),
+					'f_modificacion'  => null)
+				);
+			//$tt = ejecutores::create([
 		
-'id_p'  => 2,
-'id_p'  => 18697,
-'cargo'  => "SUBDIRECTOR DE EJECUCIÓN FISCAL",
-'titulo'  =>  "LIC",
-'f_nombramiento'  => '2015-02-17',
-'id_p_otorga_nombramiento'  => 18045,
-'usuario'  => null,
-'f_alta'  => '2015-02-17',
-'f_modificacion'  => null,	
-]);
-			$tt = ejecutores::create([
-		
-		
-'id_p'  => 15,
-'id_p'  => 19063,
-'cargo'  => "DIRECTOR DE EJECUCIÓN FISCAL",
-'titulo'  =>  "LIC",
-'f_nombramiento'  => '2015-02-26',
-'id_p_otorga_nombramiento'  => 41928,
-'usuario'  => null,
-'f_alta'  => '2015-02-26',
-'f_modificacion'  => null,	
-]);
+	DB::table('ejecutores')->insert(
+            array(	'id_ejecutor' => 2, 
+					'id_p'  => 19063,
+					'cargo'  => 'DIRECTOR DE EJECUCIÓN FISCAL',
+					'titulo'  =>  'LIC',
+					'f_nombramiento'  => '2015-02-26',
+					'id_p_otorga_nombramiento'  => 41928,
+					'usuario'  => 1,
+					'f_alta'  => '2015-02-26',
+					'f_modificacion'  => null	)
+				);
 }
 
 }
