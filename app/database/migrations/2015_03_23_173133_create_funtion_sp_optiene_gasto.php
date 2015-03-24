a85c53ca9867e01a29691dd713bdf1ac754d284c
Daniel Rivera Reyes <dani.river91@gmail.com>
2015-03-24 13:01:25 -0600

    integro la funcion sp_optiene_gasto

---
+++ app/database/migrations/2015_03_23_173133_create_funtion_sp_optiene_gasto.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFuntionSpOptieneGasto extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$existe = DB::select("select proname,pg_get_function_arguments(oid) from pg_proc where proname ='sp_optiene_gasto'");
+        
+                if(count($existe) > 0) {
+                    
+                    DB::statement("DROP FUNCTION IF EXISTS sp_optiene_gasto( IN p_gid TEXT, IN monto TEXT)");
+                }
+                    $funtion= <<<FinSP
+                               
+CREATE OR REPLACE FUNCTION sp_optiene_gasto (
+ IN p_gid TEXT,
+ IN monto TEXT
+) 
+RETURNS TABLE (procentaje INTEGER) 
+AS 
+$$
+DECLARE
+ v_consulta TEXT;
+BEGIN
+
+v_consulta := ' 
+SELECT 
+(gastos_ejecucion_porcentaje  * '''||monto||''')/100
+FROM configuracion_municipal 
+WHERE municipio ='''||p_gid||'''';
+
+RETURN QUERY EXECUTE v_consulta;
+
+END;
+$$
+LANGUAGE 'plpgsql' VOLATILE;
+                            
+FinSP;
+DB::connection()->getPdo()->exec($funtion);                           
+                
+        }           
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
