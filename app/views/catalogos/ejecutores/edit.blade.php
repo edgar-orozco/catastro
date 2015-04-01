a0f9c747e61e182136186e05d857fc109137944c
landylu <landyggonzalez@gmail.com>
2015-04-01 17:31:22 -0600

    quitando el agregar repetido en el form de ejecutores

--- app/views/catalogos/ejecutores/edit.blade.php
+++ app/views/catalogos/ejecutores/edit.blade.php
@@ -16,9 +16,9 @@
         {{ Form::model($ejecutores, ['route' => array('catalogos.ejecutores.update', $ejecutores->id_ejecutor ), 'method'=>'put' ]) }}
 
         @include('catalogos.ejecutores._form')
-<a data-toggle="modal"  data-target="#quien"  href="/catalogos/nombrador">
+<!--<a data-toggle="modal"  data-target="#quien"  href="/catalogos/nombrador">
             <span class="glyphicon glyphicon-plus"></span>
-        </a>
+        </a>-->
         <div class="form-actions form-group">
             {{ Form::submit('Modificar Ejecutores', array('class' => 'btn btn-primary')) }}
             <a href="{{URL::route('catalogos.ejecutores.index')}}" class="btn btn-warning" role="button"> Cancelar</a>
