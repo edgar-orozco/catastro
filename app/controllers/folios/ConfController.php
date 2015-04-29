25c0354fcbcf65a19e2bf7bea583df19b2ea6873
deltacastro <abel.castro9111@gmail.com>
2015-04-29 16:51:00 -0500

    Agrego la vista configuraciones y formatoreportemensual

--- app/controllers/folios/ConfController.php
+++ app/controllers/folios/ConfController.php
@@ -3,23 +3,23 @@
 class folios_ConfController extends BaseController {
 	public function index(){
 		
-		$conf = Conf::first();
+		$conf = FoliosConf::first();
 
-		return View::make('configuraciones.configuraciones', ['conf' => $conf]);
+		return View::make('folios.configuraciones.configuraciones', ['conf' => $conf]);
 
 	}
 		public function modificar(){
 			
-			$modconfig = Conf::find(1);
+			$modconfig = FoliosConf::find(1);
 			
-			$modconfig -> salario_minimo = Input::get('salario_minimo');
-			$modconfig -> director_general = Input::get('director_general');
-			$modconfig -> director_catastro = Input::get('director_catastro');
-			$modconfig -> Salario_folio_urbano = Input::get('valor_urbano');
-			$modconfig -> Salario_folio_rustico = Input::get('valor_rustico');
-			$modconfig -> ano_folio = Input::get('año_folio');
-			$modconfig -> frase_anual = Input::get('frase_anual');
-			$modconfig -> save();
+			$modconfig->salario_minimo = Input::get('salario_minimo');
+			$modconfig->director_general = Input::get('director_general');
+			$modconfig->director_catastro = Input::get('director_catastro');
+			$modconfig->salario_folio_urbano = Input::get('valor_urbano');
+			$modconfig->salario_folio_rustico = Input::get('valor_rustico');
+			$modconfig->ano_folio = Input::get('año_folio');
+			$modconfig->frase_anual = Input::get('frase_anual');
+			$modconfig->save();
 
 			return Redirect::to('/configuraciones');
 	}
---
+++ app/views/folios/folios/formatoreportemensual.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Reporte Mensual de Folios</title>
+</head>
+<body>
+<center><img src="imagenes/EncabezadoDC.jpg" WIDTH=400 HEIGHT=80 ></
+	<?php
+		$fecha = array();
+		$fecha['1'] = "Enero";
+		$fecha['2'] = "Febrero";
+		$fecha['3'] = "Marzo";
+		$fecha['4'] = "Abril";
+		$fecha['5'] = "Mayo";
+		$fecha['6'] = "Junio";
+		$fecha['7'] = "Julio";
+		$fecha['8'] = "Agosto";
+		$fecha['9'] = "Septiembre";
+		$fecha['0'] = "Octubre";
+		$fecha['11'] = "Noviembre";
+		$fecha['12'] = "Diciembre";
+	
+	?>
+
+<h1>Reporte de Folios mensual</h1>
+
+<div class="panel panel-primary">
+
+	<div class="panel-body">
+
+
+	<table border="1" WIDTH="100%">
+			<thead>
+				<tr>
+					<th>Mes</th>
+					<th>Urbanos</th>
+					<th>Rusticos</th>
+					<th>Total del Mes</th>
+					<th>Acumulado</th>
+					</tr>
+			</thead>
+			<tbody>
+			<?php $acum = 0;?>
+				@foreach($folios_historial as $reporte)
+					<tr>
+						<td align="center" >{{$fecha[$reporte->mes]}}</td>
+						<td align="center" >{{$reporte->urbano}}</td>
+						<td align="center" >{{$reporte->rustico}}</td>
+						<td align="right" >$ {{number_format($reporte->total,2)}}</td>
+						<?php $acum = $acum + $reporte->total;?>
+						<td align="right" >$ {{number_format($acum,2)}}</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+	
+	</div>
+</div>
+</body>
+</html>
