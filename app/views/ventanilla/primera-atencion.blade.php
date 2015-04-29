0ab0096b5a88a42d30218919026c63ca79c13d62
GeoDistortion <geo.distor@gmail.com>
2015-04-29 00:08:30 -0500

    GEO - CSS Styles

--- app/views/ventanilla/primera-atencion.blade.php
+++ app/views/ventanilla/primera-atencion.blade.php
@@ -35,7 +35,7 @@
 
                         <!-- Inputs de clave o cuenta -->
                         <div class="row ">
-                            <div class="col-md-6">
+                            <div>
                                 <div class="form-group">
                                     <div class="input-group">
                                         <!-- Select clave o cuenta -->
@@ -106,7 +106,7 @@
                             </div>
 
                             <!-- /col-md-6 -->
-                            <div class="col-md-6">
+                            <div>
                                 <div class="alert alert-danger" style="display: none;">
                                     No se encontró el predio solicitado en el padrón.
                                 </div>
