640bd8d3b1617dedbb60caedbaaa69eb189a5c47
Edgar Orozco <edgar.orozco@bxd.mx>
2015-04-30 01:19:47 -0500

    Se agrega nombre de servidor staging como local para evitar brincos de base por actualizaciones.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('catastro-dev'),
+	'local' => array('catastro-dev', 'catastro-staging'),
 
 ));
 
