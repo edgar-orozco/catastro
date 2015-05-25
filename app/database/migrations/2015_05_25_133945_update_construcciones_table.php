ae26066e8b752cbe7935a12a6be85383e7a6d545
dinitalaguna <dinitalaguna@gmail.com>
2015-05-25 13:58:36 -0500

    Se actualiza la tabla de construcciones
    Se actualiza la tabla de construcciones, se elimina el not null del
    campo sup_const

---
+++ app/database/migrations/2015_05_25_133945_update_construcciones_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateConstruccionesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Se elimina el not null de la columna sup_const
+                DB::statement('ALTER table construcciones ALTER COLUMN sup_const drop not null');
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//Se agrega el not null de la columna sup_const
+                DB::statement('ALTER table construcciones ALTER COLUMN sup_const set not null');
+
+	}
+
+}
