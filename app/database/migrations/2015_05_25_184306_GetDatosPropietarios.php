d2f4c579103d04b7d618c363d015aa71fa88cbc0
Edgar Orozco <edgar.orozco@bxd.mx>
2015-05-28 14:07:23 -0500

    Se compone error de duplicidad de nombre de clase.

--- app/database/migrations/2015_05_25_184306_GetDatosPropietarios.php
+++ app/database/migrations/2015_05_25_184306_GetDatosPropietarios.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class GetDatosPropietarios extends Migration {
+class GetDatosPropietariosUpdate extends Migration {
 
 	/**
 	 * Run the migrations.
