7ead75c5029335933c94710d8472ef1446db2fb7
lactobacilo <melinka@hotmail.com>
2015-06-02 23:15:14 -0500

    Se cambian nombres de campos
    Los nombres de los campos se pasan a arreglos.
    Se cambian campos de radio para tipo persona.

--- app/views/ofvirtual/notario/traslado/_form.blade.php
+++ app/views/ofvirtual/notario/traslado/_form.blade.php
@@ -33,12 +33,12 @@
  </div>
 
    {{-- {{Form::label('clave','Clave')}}--}}
-    {{Form::hidden('clave', $predio->clave, ['class'=>'form-control'])}}
-    {{$errors->first('clave', '<span class=text-danger>:message</span>')}}
+    {{Form::hidden('traslado[clave]', $predio->clave, ['class'=>'form-control'])}}
+    {{$errors->first('traslado[clave]', '<span class=text-danger>:message</span>')}}
 
    {{-- {{Form::label('cuenta','Cuenta')}}--}}
-    {{Form::hidden('cuenta', $predio->cuenta, ['class'=>'form-control'])}}
-    {{$errors->first('cuenta', '<span class=text-danger>:message</span>')}}
+    {{Form::hidden('traslado[cuenta]', $predio->cuenta, ['class'=>'form-control'])}}
+    {{$errors->first('traslado[cuenta]', '<span class=text-danger>:message</span>')}}
 
 
     {{--Vendedor --}}
@@ -52,38 +52,34 @@
 
     <div class="">
         {{Form::label('tipo_persona','Tipo de persona', ['class'=>''])}}
-        <div class="btn-group btn-toggle" data-toggle="buttons">
-            <label class="btn btn-sm btn-default active" style="width: 49%;">
-                <input type="radio" name="vendedor_tipo_persona" class="vendedor-radio-persona"
-                       value="F" checked> Física
-            </label>
-            <label class="btn btn-sm btn-default"  style="width: 49%;">
-                <input type="radio" name="vendedor_tipo_persona" class="vendedor-radio-persona"
-                       value="M"> Moral
-            </label>
-        </div>
-    </div>
+        <br>
+        {{Form::label('tipo_persona','Física', ['class'=>''])}}
+        {{ Form::radio('traslado[vendedor_tipo]', 'F', NULL, ['id'=>'vendedor-radio-persona-f','class'=>'vendedor-radio-persona']) }}
+        {{Form::label('tipo_persona','Moral', ['class'=>''])}}
+        {{ Form::radio('traslado[vendedor_tipo]', 'M', NULL, ['id'=>'vendedor-radio-persona-m','class'=>'vendedor-radio-persona']) }}
+
+      </div>
 
     {{--{{Form::label('vendedor_tipo','Vendedor Tipo')}}
     {{Form::text('vendedor_tipo', null, ['class'=>'form-control', 'autofocus'=> 'autofocus'] )}}
     {{$errors->first('vendedor_tipo', '<span class=text-danger>:message</span>')}}--}}
 
 
-     {{Form::label('vendedor_nombres','Nombre', ['class'=>''])}}
-     {{Form::text('vendedor_nombres', null, ['class' => 'form-control', 'required'=>true] )}}
+     {{Form::label('vendedor[nombres]','Nombre', ['class'=>''])}}
+     {{Form::text('vendedor[nombres]', null, ['class' => 'form-control', 'required'=>true] )}}
 
  <span class="vendedor-campos-fisica">
-    {{Form::label('vendedor_apellido_paterno','Apellido Paterno', ['class'=>''])}}
-    {{Form::text('vendedor_apellido_paterno', null, ['class' => 'form-control'] )}}
+    {{Form::label('vendedor[apellido_paterno]','Apellido Paterno', ['class'=>''])}}
+    {{Form::text('vendedor[apellido_paterno]', null, ['class' => 'form-control'] )}}
 
-    {{Form::label('vendedor_apellido_materno','Apellido Materno', ['class'=>''])}}
-    {{Form::text('vendedor_apellido_materno', null, ['class' => 'form-control'] )}}
+    {{Form::label('vendedor[apellido_materno]','Apellido Materno', ['class'=>''])}}
+    {{Form::text('vendedor[apellido_materno]', null, ['class' => 'form-control'] )}}
 
-    {{Form::label('vendedor_curp','CURP', ['class'=>''])}}
-    {{Form::text('vendedor_curp', null, ['class' => 'form-control', 'minlength'=>'18', 'maxlength'=>'18'] )}}
+    {{Form::label('vendedor[curp]','CURP', ['class'=>''])}}
+    {{Form::text('vendedor[curp]', null, ['class' => 'form-control', 'minlength'=>'18', 'maxlength'=>'18'] )}}
 </span>
-    {{Form::label('vendedor_rfc','RFC', ['class'=>''])}}
-    {{Form::text('vendedor_rfc', null, ['class' => 'form-control', 'minlength'=>'12', 'maxlength'=>'13'] )}}
+    {{Form::label('vendedor[rfc]','RFC', ['class'=>''])}}
+    {{Form::text('vendedor[rfc]', null, ['class' => 'form-control', 'minlength'=>'12', 'maxlength'=>'13'] )}}
     {{--/Vendedor --}}
 </div>
 
@@ -93,33 +89,29 @@
     <h1> Comprador </h1>
 
     <div class="">
-        {{Form::label('tipo_persona','Tipo de persona', ['class'=>''])}}
-        <div class="btn-group btn-toggle" data-toggle="buttons">
-            <label class="btn btn-sm btn-default active" style="width: 49%;">
-                <input type="radio" name="comprador_tipo_persona" class="comprador-radio-persona"
-                       value="F" checked> Física
-            </label>
-            <label class="btn btn-sm btn-default"  style="width: 49%;">
-                <input type="radio" name="comprador_tipo_persona" class="comprador-radio-persona"
-                       value="M"> Moral
-            </label>
-        </div>
-    </div>
+            {{Form::label('tipo_persona','Tipo de persona', ['class'=>''])}}
+            <br>
+            {{Form::label('tipo_persona','Física', ['class'=>''])}}
+            {{ Form::radio('traslado[comprador_tipo]', 'F', NULL, ['class'=>'comprador-radio-persona']) }}
+            {{Form::label('tipo_persona','Moral', ['class'=>''])}}
+            {{ Form::radio('traslado[comprador_tipo]', 'M', NULL, ['class'=>'comprador-radio-persona']) }}
 
-     {{Form::label('comprador_nombres','Nombre', ['class'=>''])}}
-     {{Form::text('comprador_nombres', null, ['class' => 'form-control', 'required'=>true] )}}
+          </div>
+
+     {{Form::label('comprador[nombres]','Nombre', ['class'=>''])}}
+     {{Form::text('comprador[nombres]', null, ['class' => 'form-control', 'required'=>true] )}}
  <span class="comprador-campos-fisica">
-    {{Form::label('comprador_apellido_paterno','Apellido Paterno', ['class'=>''])}}
-    {{Form::text('comprador_apellido_paterno', null, ['class' => 'form-control'] )}}
+    {{Form::label('comprador[apellido_paterno]','Apellido Paterno', ['class'=>''])}}
+    {{Form::text('comprador[apellido_paterno]', null, ['class' => 'form-control'] )}}
 
-    {{Form::label('comprador_apellido_materno','Apellido Materno', ['class'=>''])}}
-    {{Form::text('comprador_apellido_materno', null, ['class' => 'form-control'] )}}
+    {{Form::label('comprador[apellido_materno]','Apellido Materno', ['class'=>''])}}
+    {{Form::text('comprador[apellido_materno]', null, ['class' => 'form-control'] )}}
 
-    {{Form::label('comprador_curp','CURP', ['class'=>''])}}
-    {{Form::text('comprador_curp', null, ['class' => 'form-control', 'minlength'=>'18', 'maxlength'=>'18'] )}}
+    {{Form::label('comprador[curp]','CURP', ['class'=>''])}}
+    {{Form::text('comprador[curp]', null, ['class' => 'form-control', 'minlength'=>'18', 'maxlength'=>'18'] )}}
 </span>
-    {{Form::label('comprador_rfc','RFC', ['class'=>''])}}
-    {{Form::text('comprador_rfc', null, ['class' => 'form-control', 'minlength'=>'12', 'maxlength'=>'13'] )}}
+    {{Form::label('comprador[rfc]','RFC', ['class'=>''])}}
+    {{Form::text('comprador[rfc]', null, ['class' => 'form-control', 'minlength'=>'12', 'maxlength'=>'13'] )}}
     {{--/Vendedor --}}
 </div>
 
@@ -127,49 +119,49 @@
 
 <h1>Datos del predio</h1>
 
-    {{Form::label('superficie_vendida','Superficie vendida M2')}}
-    {{Form::number('superficie_vendida', null, ['class'=>'form-control', 'min' => 0, 'step'=>'any', 'min'=>0, 'max'=>$predio->superficie_terreno] )}}
-    {{$errors->first('superficie_vendida', '<span class=text-danger>:message</span>')}}
+    {{Form::label('traslado[superficie_vendida]','Superficie vendida M2')}}
+    {{Form::number('traslado[superficie_vendida]', null, ['class'=>'form-control', 'min' => 0, 'step'=>'any', 'min'=>0, 'max'=>$predio->superficie_terreno] )}}
+    {{$errors->first('traslado[superficie_vendida]', '<span class=text-danger>:message</span>')}}
 
-    {{Form::label('superficie_construccion_vendida','Superficie construcción vendida M2')}}
-    {{Form::number('superficie_construccion_vendida', null, ['class'=>'form-control',  'min' => 0, 'step'=>'any', 'min'=>0, 'max'=>$predio->superficie_construccion] )}}
-    {{$errors->first('superficie_construccion_vendida', '<span class=text-danger>:message</span>')}}
+    {{Form::label('traslado[superficie_construccion_vendida]','Superficie construcción vendida M2')}}
+    {{Form::number('traslado[superficie_construccion_vendida]', null, ['class'=>'form-control',  'min' => 0, 'step'=>'any', 'min'=>0, 'max'=>$predio->superficie_construccion] )}}
+    {{$errors->first('traslado[superficie_construccion_vendida]', '<span class=text-danger>:message</span>')}}
 
-    {{Form::label('medidas_colindancias','Medidas colindancias')}}
-    {{Form::text('medidas_colindancias', null, ['class'=>'form-control', 'step'=>'any'] )}}
-    {{$errors->first('medidas_colindancias', '<span class=text-danger>:message</span>')}}
+    {{Form::label('traslado[medidas_colindancias]','Medidas colindancias')}}
+    {{Form::textarea('traslado[medidas_colindancias]', null, ['class'=>'form-control', 'step'=>'any'] )}}
+    {{$errors->first('traslado[medidas_colindancias]', '<span class=text-danger>:message</span>')}}
 
 </div>
 <div class="form-group col-md-6">
   <h1>Datos de la escritura precedente</h1>
 
-{{Form::label('escritura_fecha','Escritura de fecha')}}
-{{Form::input('date', 'escritura_fecha', null, ['class'=>'form-control'] )}}
-{{$errors->first('escritura_fecha', '<span class=text-danger>:message</span>')}}
+{{Form::label('traslado[escritura_fecha]','Escritura de fecha')}}
+{{Form::input('date', 'traslado[escritura_fecha]', null, ['class'=>'form-control'] )}}
+{{$errors->first('traslado[escritura_fecha]', '<span class=text-danger>:message</span>')}}
 
-{{Form::label('escritura_registro','N° registro')}}
-{{Form::text('escritura_registro', null, ['class'=>'form-control'] )}}
-{{$errors->first('escritura_registro', '<span class=text-danger>:message</span>')}}
+{{Form::label('traslado[escritura_registro]','N° registro')}}
+{{Form::number('traslado[escritura_registro]', null, ['class'=>'form-control'] )}}
+{{$errors->first('traslado[escritura_registro]', '<span class=text-danger>:message</span>')}}
 
-{{Form::label('escritura_predio','Predio')}}
-{{Form::text('escritura_predio', null, ['class'=>'form-control'] )}}
-{{$errors->first('escritura_predio', '<span class=text-danger>:message</span>')}}
+{{Form::label('traslado[escritura_predio]','Predio')}}
+{{Form::number('traslado[escritura_predio]', null, ['class'=>'form-control'] )}}
+{{$errors->first('traslado[escritura_predio]', '<span class=text-danger>:message</span>')}}
 
-{{Form::label('escritura_folio','Folio')}}
-{{Form::text('escritura_folio', null, ['class'=>'form-control'] )}}
-{{$errors->first('escritura_folio', '<span class=text-danger>:message</span>')}}
+{{Form::label('traslado[escritura_folio]','Folio')}}
+{{Form::number('traslado[escritura_folio]', null, ['class'=>'form-control'] )}}
+{{$errors->first('traslado[escritura_folio]', '<span class=text-danger>:message</span>')}}
 
-{{Form::label('escritura_volumen','Volumen')}}
-{{Form::text('escritura_volumen', null, ['class'=>'form-control'] )}}
-{{$errors->first('escritura_volumen', '<span class=text-danger>:message</span>')}}
+{{Form::label('traslado[escritura_volumen]','Volumen')}}
+{{Form::number('traslado[escritura_volumen]', null, ['class'=>'form-control'] )}}
+{{$errors->first('traslado[escritura_volumen]', '<span class=text-danger>:message</span>')}}
 
-{{Form::label('escritura_impuesto_desde','Impuesto pagado del')}}
-{{Form::input('date', 'escritura_impuesto_desde', null, ['class'=>'form-control'] )}}
-{{$errors->first('escritura_impuesto_desde', '<span class=text-danger>:message</span>')}}
+{{Form::label('traslado[escritura_impuesto_desde]','Impuesto pagado del')}}
+{{Form::input('date', 'traslado[escritura_impuesto_desde]', null, ['class'=>'form-control'] )}}
+{{$errors->first('traslado[escritura_impuesto_desde]', '<span class=text-danger>:message</span>')}}
 
-{{Form::label('escritura_impuesto_hasta','Al')}}
-{{Form::input('date', 'escritura_impuesto_hasta', null, ['class'=>'form-control'] )}}
-{{$errors->first('escritura_impuesto_hasta', '<span class=text-danger>:message</span>')}}
+{{Form::label('traslado[escritura_impuesto_hasta]','Al')}}
+{{Form::input('date', 'traslado[escritura_impuesto_hasta]', null, ['class'=>'form-control'] )}}
+{{$errors->first('traslado[escritura_impuesto_hasta]', '<span class=text-danger>:message</span>')}}
 
 
 
@@ -198,18 +190,18 @@
 
              //Cuando hay cambios en los radio buttons de los requisitos
              //vendedor
-                        $('.vendedor-radio-persona').change(function (ev) {
-                            var radio = $(this);
-                            if(radio.val() == 'F'){
-                                $('.vendedor-campos-fisica').show();
-                                $('.vendedor-tipo_persona').val('F');
-                            }
-                            else if(radio.val() == 'M')
-                            {
-                                $('.vendedor-campos-fisica').hide();
-                                $('.vendedor-tipo_persona').val('M');
-                            }
-                        });
+               $('.vendedor-radio-persona').change(function (ev) {
+                    var radio = $(this);
+                    if(radio.val() == 'F'){
+                        $('.vendedor-campos-fisica').show();
+                        $('.vendedor-tipo_persona').val('F');
+                    }
+                    else if(radio.val() == 'M')
+                    {
+                        $('.vendedor-campos-fisica').hide();
+                        $('.vendedor-tipo_persona').val('M');
+                    }
+                });
 
         });
     </script>
