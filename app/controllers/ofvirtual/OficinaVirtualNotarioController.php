4abf73258a0493775914243f6bc091801d292a90
lactobacilo <melinka@hotmail.com>
2015-06-02 23:13:32 -0500

    Cambios generales
    Se separan los campos para vendedor, persona y traslado.
    Se corrigen errores en edición.
    Se puede eliminar traslados.

--- app/controllers/ofvirtual/OficinaVirtualNotarioController.php
+++ app/controllers/ofvirtual/OficinaVirtualNotarioController.php
@@ -87,81 +87,45 @@ class OficinaVirtualNotarioController extends \BaseController {
         //ToDo: sacar notaria del usuario
         $notariaId =  1;
 
-        $variables = Input::all();
-
         $vendedor = new personas();
-
-        $vendedorApellidoPaterno  = mb_strtoupper($variables["vendedor_apellido_paterno"]);
-        $vendedorApellidoMaterno  = mb_strtoupper($variables["vendedor_apellido_materno"]);
-        $vendedorNombres  = mb_strtoupper($variables["vendedor_nombres"]);
-
-        $vendedor->apellido_paterno = $vendedorApellidoPaterno;
-        $vendedor->apellido_materno = $vendedorApellidoMaterno;
-        $vendedor->nombres = $vendedorNombres;
-        $vendedor->nombrec = $vendedorApellidoPaterno . " " . $vendedorApellidoMaterno . " " . $vendedorNombres;
-        $vendedor->rfc = $variables["vendedor_rfc"];
-        $vendedor->curp = $variables["vendedor_curp"];
+        $vendedor->fill(array_map('mb_strtoupper',(Input::get('vendedor'))));
 
         if (!$vendedor->save()) {
-            return Redirect::back()->with('error', 'Error en vendedor.');
+            return Redirect::back()->with('error', 'Error en datos del vendedor.');
         }
 
-        $compradorApellidoPaterno  = mb_strtoupper($variables["comprador_apellido_paterno"]);
-        $compradorApellidoMaterno  = mb_strtoupper($variables["comprador_apellido_materno"]);
-        $compradorNombres  = mb_strtoupper($variables["comprador_nombres"]);
 
         $comprador = new personas();
-        $comprador->apellido_paterno = $compradorApellidoPaterno;
-        $comprador->apellido_materno = $compradorApellidoMaterno;
-        $comprador->nombres = $compradorNombres;
-        $comprador->nombrec = $compradorApellidoPaterno . " " . $compradorApellidoMaterno . " " . $compradorNombres;
-        $comprador->rfc = $variables["comprador_rfc"];
-        $comprador->curp = $variables["comprador_curp"];
+        $comprador->fill(array_map('mb_strtoupper',(Input::get('comprador'))));
 
         if (!$comprador->save()) {
-            return Redirect::back()->with('error', 'Error en comprador.');
+            return Redirect::back()->with('error', 'Error en datos del comprador.');
         }
 
         $traslado = new Traslado();
-
+        $traslado->fill(array_filter(Input::get('traslado')));
         $traslado->usuario_id = $usuarioId;
         $traslado->notaria_id = $notariaId;
-        $traslado->clave = $variables['clave'];
-        $traslado->cuenta = $variables['cuenta'];
         $traslado->vendedor_id = $vendedor->id_p;
-        $traslado->vendedor_tipo = $variables['vendedor_tipo_persona'];
         $traslado->comprador_id = $comprador->id_p;
-        $traslado->comprador_tipo = $variables['comprador_tipo_persona'];
-        $traslado->superficie_vendida = $variables['superficie_vendida'];
-        $traslado->superficie_construccion_vendida = $variables['superficie_construccion_vendida'];
-        $traslado->medidas_colindancias = $variables['medidas_colindancias'];
-        $traslado->escritura_fecha = $variables['escritura_fecha'];
-        $traslado->escritura_registro = $variables['escritura_registro'];
-        $traslado->escritura_predio = $variables['escritura_predio'];
-        $traslado->escritura_folio = $variables['escritura_folio'];
-        $traslado->escritura_fecha = $variables['escritura_fecha'];
-        $traslado->escritura_volumen = $variables['escritura_volumen'];
-        $traslado->escritura_impuesto_desde = $variables['escritura_impuesto_desde'];
-        $traslado->escritura_impuesto_hasta = $variables['escritura_impuesto_hasta'];
 
         //Como usuario notario, requiero que se validen los montos de terreno a vender no sean mayores que el declarado en el registro de predio.
-
         $predio = $this->padron->getByClaveOCuenta($traslado->clave);
 
         if($traslado->superficie_vendida > $predio->superficie_terreno){
-            return Redirect::back()->with('error', 'La superficie vendida no puede ser mayor que la superficie del terreno.');
+            return Redirect::back()->withInput()->with('error', 'La superficie vendida no puede ser mayor que la superficie del terreno.');
         }
 
         if($traslado->superficie_construccion_vendida > $predio->superficie_construccion){
-            return Redirect::back()->with('error', 'La superficie de construcción vendida no puede ser mayor que la superficie de construcción del terreno');
+            return Redirect::back()->withInput()->with('error', 'La superficie de construcción vendida no puede ser mayor que la superficie de construcción del terreno');
         }
 
         if (!$traslado->save()) {
-            return Redirect::back()->withErrors($traslado->errors());
+            return Redirect::back()->withInput()->withErrors($traslado->errors());
         }
 
         //Dado que fue exitosa la actualización mostramos la salida al usuario.
-        return Redirect::to('ofvirtual/notario/traslado/create')->with('success','¡Se ha creado correctamente el traslado de dominio para la cuenta ' . $traslado->cuenta .'!');
+        return Redirect::to('ofvirtual/notario/traslado')->with('success','¡Se ha creado correctamente el traslado de dominio para la cuenta ' . $traslado->cuenta .'!');
     }
 
 
@@ -191,14 +155,15 @@ class OficinaVirtualNotarioController extends \BaseController {
 
         $predio = $this->padron->getByClaveOCuenta($traslado->clave);
 
+        //vendedor
+        $vendedor= personas::find($traslado->vendedor_id);
+        $traslado->vendedor->fill($vendedor->toArray());
 
-        $comprador = personas::find($traslado->comprador_id);
-        $vendedor = personas::find($traslado->vendedor_id);
+        //comprador
+        $comprador= personas::find($traslado->comprador_id);
+        $traslado->comprador->fill($comprador->toArray());
 
-
-        $traslado2 = (object) array_merge((array)$traslado, (array)$comprador, (array)$vendedor);
-
-print_r($traslado2);
+        $traslado->traslado = $traslado;
         // Title
         $title = 'Editar traslado de dominio';
 
@@ -215,19 +180,49 @@ print_r($traslado2);
 	 */
 	public function update($id)
 	{
-		//
-        //Buscamos el rol original relacionado con el id
-        $traslado = Traslado::find($id);
-        //Llenamos los parametros que vienen de la forma
-       $traslado->fill(Input::all());
+		//ToDo: revisar primero que no tenga asignado un folio, si tiene asigando un folio no se puede editar
 
-        //Si no puede guardarse entonces mostramos errores en pantalla
-        if ( ! $traslado->save()) {
-            return Redirect::back()->withErrors($this->traslado->errors());
+        //Buscamos el traslado original relacionado con el id
+        $traslado = Traslado::find($id);
+        //
+
+        //ToDo: checar si se puede hacer update de notaria y usuario
+        //$usuarioId = Auth::id();
+        //$notariaId =  1;
+
+        $vendedor = personas::find($traslado->vendedor_id);
+        $vendedor->fill(array_map('mb_strtoupper',(Input::get('vendedor'))));
+
+        if (!$vendedor->save()) {
+            return Redirect::back()->with('error', 'Error en datos del vendedor.');
         }
 
-        //Dado que fue exitosa la actualización mostramos la salida al usaurio.
-        return Redirect::to('ofvirtual.notario.traslado.edit'.$this->traslado->id.'/edit')->with('success','¡Se ha actualizado correctamente el rol: '. " !");
+        $comprador = personas::find($traslado->comprador_id);
+        $comprador->fill(array_map('mb_strtoupper',(Input::get('comprador'))));
+
+        if (!$comprador->save()) {
+            return Redirect::back()->with('error', 'Error en datos del comprador.');
+        }
+
+        $traslado->fill(array_filter(Input::get('traslado')));
+
+        //Como usuario notario, requiero que se validen los montos de terreno a vender no sean mayores que el declarado en el registro de predio.
+        $predio = $this->padron->getByClaveOCuenta($traslado->clave);
+
+        if($traslado->superficie_vendida > $predio->superficie_terreno){
+            return Redirect::back()->withInput()->with('error', 'La superficie vendida no puede ser mayor que la superficie del terreno.');
+        }
+
+        if($traslado->superficie_construccion_vendida > $predio->superficie_construccion){
+            return Redirect::back()->withInput()->with('error', 'La superficie de construcción vendida no puede ser mayor que la superficie de construcción del terreno');
+        }
+
+        if (!$traslado->save()) {
+            return Redirect::back()->withInput()->withErrors($traslado->errors());
+        }
+
+        //Dado que fue exitosa la actualización mostramos la salida al usuario.
+        return Redirect::to('ofvirtual/notario/traslado/edit/'.$traslado->id)->with('success','¡Se ha actualizado correctamente el traslado!');
 	}
 
 
@@ -240,6 +235,22 @@ print_r($traslado2);
 	public function destroy($id)
 	{
 		//
+        //ToDo: revisar primero que no tenga asignado un folio, si tiene asigando un folio no se puede borrar
+
+        $traslado = Traslado::find($id);
+
+        $traslado->delete();
+
+        $vendedor = personas::find($traslado->vendedor_id);
+        $vendedor->delete();
+
+        $comprador = personas::find($traslado->comprador_id);
+        $comprador->delete();
+
+        return Redirect::to('ofvirtual/notario/traslado')->with('success','¡Se ha eliminado correctamente el traslado!');
+
+
+
 	}
 
 
