12259a5b6c85b2ae6a982fc24ea91adea592fa01
David Galindo <david@Mac-mini-de-David.local>
2015-06-09 15:28:25 -0500

    1244980-96401842 Como programador requiero que el markup y el script del lockscreen pueda reutili...

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -18,6 +18,8 @@
     {{ HTML::style('css/navmenu.css') }}
     {{ HTML::style('css/header.css') }}
     {{ HTML::style('css/footer.css') }}
+    <!-- CSS del  Lock Screen -->
+    {{ HTML::style('css/lockscreen.css') }}
 
     <style>
         @yield('styles')
@@ -217,39 +219,8 @@
     </div>
 </div>
 
-<div class="lock-screen" id="lock-screen">
-    <div class="middle-box text-center lock-screen-container animated fadeInDown">
-        <div>
-            <div class="m-b-md">
-                <img alt="image" class="img-circle circle-border" src="http://3.bp.blogspot.com/-u3DEwcaMpTA/VPvwJweXz0I/AAAAAAAAAMs/wdxC1nSsC8A/s1600/contacts-logo.png">
-            </div>
-            <h3>{{Auth::user()->nombreCompleto()}}</h3>
-            <p>
-                Tu sesión se cerro por inactividad, ingresa tu contraseña para desbloquear esta pantalla.
-            </p>
-            <div id="lock-screen-error" class="alert alert-danger">
-                <span class="glyphicon glyphicon-remove"></span>
-                <span id="lock-screen-error-text"></span>
-            </div>
-            <form class="m-t" role="form" id="lock-screen-form">
-                <div class="form-group">
-                    <input type="password" id="lock-password" class="form-control" placeholder="*******" required="">
-                    <input type="hidden" id="lock-username" value="{{ Auth::user()->username }}" >
-                </div>
-                <button type="submit" id="lock-screen-submit" class="btn btn-primary btn-block">
-                    Desbloquear
-                </button>
-                <img id="lock-screen-loader" src="/mapper/images/indicator.gif" alt="cargando ...">
-            </form>
-        </div>
-        <div class="change-user">
-            <a href="/users/login">
-                <i class="glyphicon glyphicon-user"></i>
-                Cambiar de usuario
-            </a>
-        </div>
-    </div>
-</div>
+<!-- Lock Screen -->
+@include('layouts._lockscreen')
 
 <!-- JQuery -->
 {{ HTML::script('js/jquery/jquery.min.js') }}
@@ -263,6 +234,7 @@
 {{ HTML::script('js/angular/angular-sanitize.js') }}
 {{ HTML::script('js/angular/angular-animate.js') }}
 {{ HTML::script('js/ui-bootstrap.js') }}
+<!-- JS del Lock Screen -->
 {{ HTML::script('js/lockscreen.js') }}
 
 @yield('javascript')
