e40de47468fc2f3476f12e14e94798a4230b2658
Edgar Orozco <edgar.orozco@bxd.mx>
2015-06-16 01:08:06 -0500

    Se corrige nombre de clase, no corresponde al estandar manejado por Laravel ("camelcase") por lo que al ejecutarse en el servidor o ambiente de desarrollo genera un error al no encontrar el nombre correcto.

--- app/database/migrations/2015_06_15_000042_corevat_aef_construcciones_valor_nuevo.php
+++ app/database/migrations/2015_06_15_000042_corevat_aef_construcciones_valor_nuevo.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CorevatAefConstrucciones_valor_nuevo extends Migration {
+class CorevatAefConstruccionesValorNuevo extends Migration {
 
 	/**
 	 * Run the migrations.
