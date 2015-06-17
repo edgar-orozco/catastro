2b1cd12d4acafde02d893ab06a8d1d0d0b0693f0
Edgar Orozco <edgar.orozco@bxd.mx>
2015-06-17 00:57:44 -0500

    Se corrige duplicidad de nombre de clases, ya existia una clase UpdateCondominiosTable

--- app/database/migrations/2015_06_04_091609_update_condominios_table.php
+++ app/database/migrations/2015_06_04_091609_update_condominios_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateCondominiosTable extends Migration {
+class UpdateCondominiosTable2 extends Migration {
 
 	/**
 	 * Run the migrations.
