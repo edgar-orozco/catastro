f0d897574af723728a822c14b91a23ecb3817050
roman08 <rmcentinela@gmail.com>
2015-06-26 19:06:48 -0500

    se crean directorios para macros

---
+++ app/macros/macrosPersonas.php
@@ -0,0 +1,30 @@
+<?php
+Form::macro('personas', function($llave)
+{
+     $personas = HTML::script('js/macros.js').
+             Form::text($llave.'[id_tipo]',1,['id' => 'id_tipo','hidden']).
+             Form::label($llave.'[nombres]','Nombre',['id' => 'nombre']).
+             Form::text($llave.'[nombres]', null, ['class' => 'form-control'] ).
+             Form::label($llave.'[apellido_paterno]','Apellido paterno',['id' => 'apellido_paterno']).
+             Form::text($llave.'[apellido_paterno]', null, ['class' => 'form-control'] ).
+             Form::label($llave.'[apellido_materno]','Apellido materno',['id' => 'apellido_materno']).
+             Form::text($llave.'[apellido_materno]', null, ['class' => 'form-control'] ).
+             Form::label($llave.'[curp]','CURP', ['class'=>'','id' => 'curp']).
+             Form::text($llave.'[curp]', null, ['class' => 'form-control','required',
+                    'minlength'=>'18',
+                    'maxlength'=>'18',
+                    'pattern' => '([A-Za-z]{4})([0-9]{6})([A-Za-z]{6})([0-9]{2})',
+                    'title' => 'El CURP ingresado no tiene el formato esperado, verifique nuevamente el CURP ingresado',
+
+              ]).
+               Form::label($llave.'[rfc]','R.F.C', ['class'=>'']).
+             Form::text($llave.'[rfc]', null, ['class' => 'form-control','required',
+                    'minlength'=>'13',
+                    'maxlength'=>'13',
+                    'pattern' => '([a-zA-Z]{4})([0-9]{6})([a-zA-Z]{2})([0-9]{1})',
+                    'title' => 'El RFC ingresado no tiene el formato esperado, verifique nuevamente el RFC ingresado'
+              ])
+             ;
+
+    return $personas;
+});
