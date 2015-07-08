a56648bc64e19fad714f71e27b07a8e4ee8ca79e
Edgar Orozco <edgar.orozco@bxd.mx>
2015-07-08 15:37:51 -0500

    Se crea detección de tipo default.

--- public/js/macros/controlClaveCuenta.js
+++ public/js/macros/controlClaveCuenta.js
@@ -3,29 +3,40 @@
  */
 
 $(function () {
-    //Inicializamos el campo con una máscara por cuenta catastral
-    $('.clave-catastral').mask("00-S-000000", {
-        placeholder: "__-_-______",
-        translation: {S: {pattern: /[RUru]/}}
-    });
+
+    var cuenta = $('.clave-catastral').val();
+    var tipo = 'cuenta';
+    if(cuenta){
+        if(cuenta.length > 11){
+            //Se trtata de una clave catastral
+            tipo = 'clave';
+            tipoControl(tipo);
+        }
+    }
+
+    tipoControl(tipo);
 
     //Cuando se selecciona un valor del dropdown del buscador entonces:
     $('.dropdown-menu.tipo-busqueda li a').click(function () {
         var tipo = $(this).data('tipo');
         $('.select-busqueda').data('tipobusqueda', tipo);
         $('.clave-catastral').val('');
+        tipoControl(tipo);
+        $('.clave-catastral').focus();
+    });
+
+    function tipoControl(tipo){
         if (tipo == 'cuenta') {
             $('.clave-catastral').mask("00-S-000000", {
                 placeholder: "__-_-______",
                 translation: {S: {pattern: /[RUru]/}}
             });
             $('.control-clave-cuenta .dropdown-label').text('Cuenta');
-            $('.clave-catastral').focus();
         }
         else {
             $('.clave-catastral').mask("00-000-000-0000-000000", {placeholder: "__-___-___-____-______"});
             $('.control-clave-cuenta .dropdown-label').text('Clave');
-            $('.clave-catastral').focus();
         }
-    });
+    };
+
 });
