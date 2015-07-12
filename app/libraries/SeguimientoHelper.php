2124fa6c44d9c6c060f5109aa504ef3f95964352
Edgar Orozco <edgar.orozco@bxd.mx>
2015-07-11 19:11:03 -0500

    Se eliminan vocales para evitar palabras inconvenientes. Ahora la longitud de las posibles variantes es de 25

--- app/libraries/SeguimientoHelper.php
+++ app/libraries/SeguimientoHelper.php
@@ -5,7 +5,7 @@ class SeguimientoHelper{
     public static function generarClave() {
         $longitud = 6;
         $key = '';
-        $parametro = '237ABCDEFGHJKLMNPRSTUVWXYZ';
+        $parametro = '23479BCDFGHJKLMNPRSTVWXYZ';
         $max = strlen($parametro) - 1;
         for ($i = 0; $i < $longitud; $i++)
         $key .= $parametro{mt_rand(0, $max)};
