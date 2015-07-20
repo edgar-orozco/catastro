443139433843bbc97802d043a1e6d1876f71f02f
lactobacilo <melinka@hotmail.com>
2015-07-19 23:08:24 -0500

    Se revisa si la cadena de seguimiento es unica antes de guardar.

--- app/models/Traslado.php
+++ app/models/Traslado.php
@@ -71,6 +71,42 @@ class Traslado extends Eloquent
     }*/
 
     /**
+     * Regresa una cadena de seguimiento unica dentro del rango de tiempo de expiración predeterminado (un año)
+     * @param null $cadena
+     * @return string
+     */
+    public static function cadenaSeguimientoUnica($cadena = null){
+
+        //Si la cadena es nula, es decir se ha llamado SolicitudGestion::cadenaSeguimientoUnica() entonces generamos una cadena.
+        if(!$cadena)
+        {
+            $cadena = SeguimientoHelper::generarClave();
+        }
+
+        //Consultamos si es que ya existe un registro con la misma cadena dentro del rango de un año
+        $haceunanio = date("Y-m-d", strtotime('-1 year'));
+
+        //Hacemos la consulta a ver si ya existe:
+        $solicitud = SolicitudGestion::where('seguimiento',$cadena)
+            // Aqui se debe consultar desde hoy hasta un año atras
+            ->where('create_at', '>', $haceunanio)
+            ->first();
+
+        //Si existe una solicitud con la misma clave dentro del rango de tiempo del año pasado, entonces volvemos a crear una nueva cadena
+        //y volvemos a preguntar si existe:
+        if($solicitud)
+        {
+            $cadena = SeguimientoHelper::generarClave();
+            return SolicitudGestion::cadenaSeguimientoUnica($cadena);
+        }
+        else
+        {
+            //Si no existe la solicitud con la misma cadena de segumiento, entonces regresamos la cadena.
+            return $cadena;
+        }
+    }
+
+    /**
      * Scope para consultar  traslados por el nombre del vendedor, puede consultar por el nombre exacto o por nombre parcial (una fraccion del nombre)
      * @param $q
      * @param $nombre
