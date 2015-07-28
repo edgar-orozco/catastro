edda49f1f5c9a839be51fd5d2326af1cb7e56640
carlosAgustin <carlos.agustin@ticase.com.mx>
2015-07-28 04:41:22 +0100

    COREVAT: Correción en la datatable de Medidas y Colindancias.

--- app/models/AiMedidasColindancias.php
+++ app/models/AiMedidasColindancias.php
@@ -53,7 +53,7 @@ class AiMedidasColindancias extends \Eloquent {
 		$row = new AiMedidasColindancias();
 		$row->idavaluoinmueble = $inputs['idavaluoinmueble2'];
 		$row->idorientacion = $inputs['idorientacion'];
-		//$row->medidas = $inputs['medidas'];
+		$row->medidas = $inputs['medidas'];
 		$row->unidad_medida = $inputs['unidad_medida'];
 		$row->colindancia = $inputs['colindancia'];
 		$row->idemp = 1;
