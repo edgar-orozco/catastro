cbba5cc8278b25b200283609ef695af68c87a731
Geo Distor <geodistor@MacBook-Pro-de-Geo.local>
2015-07-31 00:14:45 -0500

    Geo Style CHanges

--- app/views/Corevat/Avaluos/_general.blade.php
+++ app/views/Corevat/Avaluos/_general.blade.php
@@ -62,20 +62,22 @@
 </style>
 {{ Form::model($row, ['route' => array('updateAvaluoGeneral', $row->idavaluo), 'method'=>'post' ]) }}
 <div class="row coveratCont">
-	<div class="col-md-4">
+	<div class="col-md-12">
 		<div class="form-group">
 			{{Form::label('fecha_reporte', 'Fecha del Reporte', ['class'=>'col-md-2'])}}
-            <div class="col-md-12">
+            <div class="col-md-10">
                 {{Form::text('fecha_reporte', $row->fecha_reporte, ['class'=>'form-control', 'tabindex'=>'1', 'autofocus' => 'autofocus', 'required' => 'required', 'maxlength' => '10', 'size' => '11', 'style' => 'width:110px', 'readonly'=>'readonly'])}}
                 {{$errors->first('fecha_reporte', '<span class=text-danger>:message</span>')}}
             </div>
 		</div>
 	</div>
-	<div class="col-md-4">
+	<div class="col-md-12">
 		<div class="form-group">
-			{{Form::label('fecha_avaluo', 'Fecha del Avalúo')}}
-			{{Form::text('fecha_avaluo', $row->fecha_avaluo, ['class'=>'form-control', 'tabindex'=>'2', 'required' => 'required', 'maxlength' => '10', 'size' => '11', 'style' => 'width:110px', 'readonly'=>'readonly'])}}
+			{{Form::label('fecha_avaluo', 'Fecha del Avalúo', ['class'=>'col-md-2'])}}
+            <div class="col-md-10">
+            {{Form::text('fecha_avaluo', $row->fecha_avaluo, ['class'=>'form-control', 'tabindex'=>'2', 'required' => 'required', 'maxlength' => '10', 'size' => '11', 'style' => 'width:110px', 'readonly'=>'readonly'])}}
 			{{$errors->first('fecha_avaluo', '<span class=text-danger>:message</span>')}}
+            </div>
 		</div>
 	</div>
 	<div class="col-md-4">
