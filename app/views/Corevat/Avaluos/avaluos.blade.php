41f9453539850b68fa724163bc0c7ec40e26394b
Geo Distor <geodistor@MacBook-Pro-de-Geo.local>
2015-07-31 01:40:28 -0500

    Geo Style CHanges

--- app/views/Corevat/Avaluos/avaluos.blade.php
+++ app/views/Corevat/Avaluos/avaluos.blade.php
@@ -5,7 +5,7 @@
 {{ HTML::style('/js/jquery/jquery-ui.css') }}
 	<h1>Avalúos</h1>
 	<hr>
-	<p>
+	<div>
 		<a class="btn btn-info btn0" id="btn1General" href="{{ action('editAvaluoGeneral',['id'=>$idavaluo])}}" role="button">General</a>
 		<a class="btn btn-info btn0" id="btn2Zona" href="{{ action('editAvaluoZona',['id'=>$idavaluo])}}" role="button">Zona</a>
 		<a class="btn btn-info btn0" id="btn3Inmueble" href="{{ action('editAvaluoInmueble',['id'=>$idavaluo])}}" role="button">Inmueble</a>
@@ -14,7 +14,7 @@
 		<a class="btn btn-info btn0" id="btn3Conclusion" href="{{ action('editAvaluoConclusiones',['id'=>$idavaluo])}}" role="button">Conclusiones</a>
 		<a class="btn btn-info btn0" id="btn3FotoPlano" href="{{ action('editAvaluoFotos',['id'=>$idavaluo])}}" role="button">Fotos y Planos</a>
 		<a class="btn btn-info btn0" id="btn3PrintAvaluo" href="/corevat/AvaluoPrint/{{$row->idavaluo}}" target="_blank" role="button">Imprimir</a>
-	</p>
+	</div>
 	<hr>
 	<div id="conatinerAvaluo" style="margin: 0 !important; padding: 0px !important;">
 		@if ( $opt === 'general' )
