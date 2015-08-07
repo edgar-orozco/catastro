832321a64fe167b538e15ebdaf76dc2cc9f400f1
Geo Distor <geodistor@MacBook-Pro-de-Geo.local>
2015-08-07 18:19:20 -0500

    Geo - Inmuebles

--- app/views/Corevat/Avaluos/_inmueble.blade.php
+++ app/views/Corevat/Avaluos/_inmueble.blade.php
@@ -32,7 +32,7 @@
 	<div class="col-md-11"><h3>Medidas y Colindancias</h3></div>
 	<div class="col-md-1"><a class="btn nuevo" id="btnNew" title="Nuevo Avaluo">Nuevo</a></div>
 
-	<div class="col-md-12 bg-primary"><h4>Detalles de Medidas y Colindancias</h4></div>
+	<div class="col-md-12"><h4>Detalles de Medidas y Colindancias</h4></div>
 	<div class="col-md-12">
 		<table cellpadding="0" cellspacing="0" border="0" class="table datatable table-striped corevatDataTable" id="ai_medidas_colindanciase-table">
 			<thead>
