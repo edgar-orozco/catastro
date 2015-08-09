983088f0386fd1f4433607e05afcd6731e7873ca
Geo Distor <geodistor@MacBook-Pro-de-Geo.local>
2015-08-08 20:19:33 -0500

    css coverat - Geo

--- app/views/Corevat/Avaluos/index.blade.php
+++ app/views/Corevat/Avaluos/index.blade.php
@@ -55,7 +55,7 @@
             <tbody>
             @foreach ($rows as $row)
                 <tr>
-                    <td><a href="{{ action('corevat_AvaluosController@editGeneral', ['id'=>$row->idavaluo] )}}" class="btn btn-xs btn-info" title="Editar">{{$row->idavaluo}}</a></td>
+                    <td class="idEdit"><a href="{{ action('corevat_AvaluosController@editGeneral', ['id'=>$row->idavaluo] )}}" class="btn btn-xs btn-info" title="Editar">{{$row->idavaluo}}</a></td>
                     <td>{{$row->foliocoretemp}}</td>
                     <td>{{$row->fecha_avaluo}}</td>
                     <td>{{$row->nombre_solicitante}}</td>
