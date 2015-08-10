5c56a41c8b821d929d9112cc81556b5c70d82af0
Geo Distor <geodistor@MacBook-Pro-de-Geo.local>
2015-08-09 23:40:14 -0500

    css coverat - Geo

--- app/views/Corevat/Avaluos/_inmueble.blade.php
+++ app/views/Corevat/Avaluos/_inmueble.blade.php
@@ -341,10 +341,10 @@
 		<div class="col-md-12">&nbsp;</div>
 		<hr>
         <div class="col-md-6 form-actions">
-            <a href="{{URL::route('indexAvaluos')}}" class="btn btn-coveratPrincipal" role="button"><i class="glyphicon glyphicon-arrow-left"></i> Regresar</a>
+            <a href="{{URL::route('indexAvaluos')}}" class="btn btn-coveratSecondary" role="button"><i class="glyphicon glyphicon-arrow-left"></i> Regresar</a>
         </div>
         <div class="col-md-6 form-actions">
-            {{Form::submit('Guardar', ['class'=>'btn btn-coveratSecondary'])}}
+            {{Form::submit('Guardar', ['class'=>'btn btn-coveratPrincipal'])}}
         </div>
 	</div>
 </div>
