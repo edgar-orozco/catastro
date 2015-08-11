4d123aea5ef8d7b656a8bd1d346fb6e8e3bf1211
vagrant <landyggonzalez@gmail.com>
2015-08-11 12:28:27 -0500

    se modifica el ws de inpc

--- app/libraries/IndicadoresHelper.php
+++ app/libraries/IndicadoresHelper.php
@@ -1,27 +1,30 @@
 <?php
 
-class IndicadoresHelper
-{
-     public function getinpc() {
+class IndicadoresHelper {
 
+    public static function getinpc() {
         $client = new SoapClient("http://www2.inegi.org.mx/servicioindicadores/Indicadores.asmx?WSDL");
         $param = array(
             'Indicador' => '216064',
             'ubicacionGeografica' => '27',
         );
         $ready = $client->obtieneValoresOportunos($param)->obtieneValoresOportunosResult;
-        $xml = simplexml_load_string($ready->any);
+        // $xml = simplexml_load_string($ready->any);
+        $array = explode(" ", $ready->any);
+        $periodo = explode('"', $array[24]);
+        $array = explode('"', $array[25]);
+        $inpc = $array[1];
+        $per = $periodo[1];
+        $time = explode("/", $per);
+        $anio = $time[0];
+        $mes = $time[1];
 
-
-
-//
-//        $resultXML = simplexml_load_string($result);
-//        $item = $resultXML->obtieneValoresOportunosResponse->obtieneValoresOportunosResul;
-        echo "<pre>";
-        print_r($xml);
- echo "</pre>";
-
-        $vista = View::make('PDF-RegistroEscritura');
-        return $vista;
+        $arrayinpc = array(
+            'año' => $anio,
+            'mes' => $mes,
+            'inpc' => $inpc,
+        );
+       return $arrayinpc;
     }
+
 }
