905a415f14aeeba88cc55c5d4e8df7227c5c17b7
Becerro <carlos.agustin@ticase.com.mx>
2015-08-15 10:23:22 +0100

    COREVAT: Enfoque físico, ventanas modales y editar factor conservando los combitos

---
+++ app/database/migrations/2015_08_14_000001_CorevatAlterTableAefTerrenosDos.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CorevatAlterTableAefTerrenosDos extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::connection('corevat')->table('aef_terrenos', function(Blueprint $table) {
+			$table->integer('fk_top')->default(0)->nullable();
+			$table->integer('fk_otros')->default(0)->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::connection('corevat')->table('aef_terrenos', function(Blueprint $table) {
+			$table->dropColumn('fk_top');
+			$table->dropColumn('fk_otros');
+		});
+	}
+
+}
