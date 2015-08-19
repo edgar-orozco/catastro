d9f2b9390d1acd78128c2ec64e9ed43b25634d43
Geo Distor <geodistor@MacBook-Pro-de-Geo.local>
2015-08-19 14:30:23 -0500

    Geo - terms changes

--- app/views/ventanilla/flujo.blade.php
+++ app/views/ventanilla/flujo.blade.php
@@ -11,16 +11,16 @@
 
     <div class="row clearfix">
         @if($folio)
-            <div class="col-md-2 col-md-offset-4">
+            <div class="col-md-2 col-md-offset-4" style="border: 1px solid darkgray">
                 <h4>Folio: {{$anio}}/{{$municipio}}/{{sprintf("%06d",$folio)}}</h4>
             </div>
 
-            <div class="col-md-3 ">
+            <div class="col-md-3 "  style="border: 1px solid darkgray">
                 <h4>Consumidos: {{$tiempo_transcurrido}} de {{$tiempo_tramite}} días</h4>
             </div>
 
-            <div class="col-md-3">
-                <h4>Estado: <span class="alert alert-success">{{$tramite->estatus->pasado}}</span></h4>
+            <div class="col-md-3"  style="border: 1px solid darkgray">
+                <h4>Estado: <span class="alert alert-success" style="padding: 5px 43px;">{{$tramite->estatus->pasado}}</span></h4>
             </div>
         @endif
     </div>
