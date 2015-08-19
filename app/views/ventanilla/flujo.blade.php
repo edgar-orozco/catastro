86f4f5df083e161c4efc8dbf87c5d05a08ac526c
Geo Distor <geodistor@MacBook-Pro-de-Geo.local>
2015-08-19 14:38:02 -0500

    Geo - terms changes

--- app/views/ventanilla/flujo.blade.php
+++ app/views/ventanilla/flujo.blade.php
@@ -13,24 +13,6 @@
 
     <div class="row clearfix">
 
-        <div>
-            @if($folio)
-                <div class="col-md-4" style="border: 1px solid darkgray">
-                    <h4>Folio: {{$anio}}/{{$municipio}}/{{sprintf("%06d",$folio)}}</h4>
-                </div>
-
-                <div class="col-md-4 "  style="border: 1px solid darkgray">
-                    <h4>Consumidos: {{$tiempo_transcurrido}} de {{$tiempo_tramite}} días</h4>
-                </div>
-
-                <div class="col-md-4"  style="border: 1px solid darkgray">
-                    <h4>Estado: <span class="alert alert-success" style="padding: 5px 43px;">{{$tramite->estatus->pasado}}</span></h4>
-                </div>
-            @endif
-        </div>
-
-
-
         <?php $nocontrol = false;?>
         @if(($tramite->estatus->pasado != 'Finalizado' && $tramite->estatus->pasado != 'Finalizado observado' ) && $esResponsable != null)
 
@@ -92,7 +74,21 @@
         @else
         <div class="col-md-8 column">
         @endif
+            <div style="padding: 10px 0">
+                @if($folio)
+                    <div class="col-md-4" style="border: 1px solid darkgray">
+                        <h4>Folio: {{$anio}}/{{$municipio}}/{{sprintf("%06d",$folio)}}</h4>
+                    </div>
 
+                    <div class="col-md-4 "  style="border: 1px solid darkgray">
+                        <h4>Consumidos: {{$tiempo_transcurrido}} de {{$tiempo_tramite}} días</h4>
+                    </div>
+
+                    <div class="col-md-4"  style="border: 1px solid darkgray">
+                        <h4>Estado: <span class="alert alert-success" style="padding: 5px 43px;">{{$tramite->estatus->pasado}}</span></h4>
+                    </div>
+                @endif
+            </div>
 
             <div class="tabbable" id="tabs">
 
