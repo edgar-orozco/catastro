f75a06769f81b3ed3a2051496d7d10030b3c7b5c
Geo Distor <geodistor@MacBook-Pro-de-Geo.local>
2015-08-21 02:24:51 -0500

    Geo css Kiosko changes

--- app/views/folios/entregafoliosm/urbanos.blade.php
+++ app/views/folios/entregafoliosm/urbanos.blade.php
@@ -11,79 +11,83 @@
 
 <div class="panel panel-primary">
 
+    <div class="panel-body">
+        {{Form::open(['id' => 'foliosMU', 'method' => 'GET'])}}
+        {{Form::select('year', $selectYear, null,  ['id' => 'year', 'class' => 'form-control input-sm', 'aria-controls' => 'emitidos-table'])}}
+        {{Form::close()}}
+        <div class="row">
+            <div class="col-md-3">
+                <label>COREVAT</label>
+                <h5>{{$perito->corevat}}</h5>
+            </div>
+            <div class="col-md-3">
+                <label>NOMBRE</label>
+                <h5>{{$perito->nombre}}</h5>
+            </div>
+        </div>
+        {{Form::open()}}
+        <table class="table datatable" id="example">
+            <thead>
+            <tr>
+                <!--<th>{{Form::checkbox('', '', '', ['id'=>'todos'])}}</th>-->
+                <th>Marcar Folio Presentado</th>
+                <th>Folio Autorizados</th>
+                <th>Folio usado en el Municipio de:</th>
+                <th>Fecha de Recepción</th>
+                <th>Estado del Folio</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($fu as $u)
+
+
+                <tr>
+                    <td align="center">
+                        @if($u->entrega_municipal == 0)
+                            {{Form::checkbox('urbanos[]', $u->numero_folio, '', ['class'=>'checkbox'])}}
+                        @else
+                            <i class="glyphicon glyphicon-ok"></i>
+                        @endif
+                    </td>
+                    <?php
+                    $input = $u->numero_folio;
+                    $input = str_pad($input, 4, "0", STR_PAD_LEFT);
+                    ?>
+                    <td align="center"> {{$perito->corevat."-".$input.$u->tipo_folio."-15"}}</td>
+                    <td align="center">
+                        @if($u->entrega_municipal == 1)
+                            {{$u->municipio->nombres_municipio}}
+                        @endif
+                    </td>
+                    <td align="center">
+                        @if($u->entrega_municipal == 1)
+                            {{$u->fecha_entrega_m}}
+                        @endif
+                    </td>
+                    <td align="center">
+                        @if($u->entrega_municipal == 0)
+                            Vigente
+                        @else
+                            Usado
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        <div class="row">
+            <div class="col-md-6">
+                {{Form::submit('Guardar', ['class'=>'btn btn-block btn-success'])}}
+                {{Form::close()}}
+            </div>
+        </div>
+    </div>
+
 	<div class="panel panel-default">
 
 		<h2 class="panel-title">FOLIOS URBANOS AUTORIZADOS PARA</h2>
 
-	</div>
-
-	<div class="panel-body">
-	{{Form::open(['id' => 'foliosMU', 'method' => 'GET'])}}
-	{{Form::select('year', $selectYear, null,  ['id' => 'year', 'class' => 'form-control input-sm', 'aria-controls' => 'emitidos-table'])}}
-	{{Form::close()}}
-		<div class="row">
-			<div class="col-md-3">
-				<label>COREVAT</label>
-				<h5>{{$perito->corevat}}</h5>
-			</div>
-			<div class="col-md-3">
-				<label>NOMBRE</label>
-				<h5>{{$perito->nombre}}</h5>				
-			</div>
-		</div>
-		{{Form::open()}}
-		<table class="table datatable" id="example">
-			<thead>
-				<tr>
-					<!--<th>{{Form::checkbox('', '', '', ['id'=>'todos'])}}</th>-->
-					<th>Marcar Folio Presentado</th>
-					<th>Folio Autorizados</th>
-					<th>Folio usado en el Municipio de:</th>
-					<th>Fecha de Recepción</th>
-					<th>Estado del Folio</th>
-
-				</tr>
-			</thead>
-			<tbody>
-				@foreach($fu as $u)
-
-					
-				<tr>
-					<td align="center"> 
-						@if($u->entrega_municipal == 0)
-							{{Form::checkbox('urbanos[]', $u->numero_folio, '', ['class'=>'checkbox'])}}
-						@else
-							<i class="glyphicon glyphicon-ok"></i>
-						@endif
-					</td>
-					<?php
-						$input = $u->numero_folio;
-						$input = str_pad($input, 4, "0", STR_PAD_LEFT);
-					?>
-					<td align="center"> {{$perito->corevat."-".$input.$u->tipo_folio."-15"}}</td>
-					<td align="center"> 
-					@if($u->entrega_municipal == 1)
-						{{$u->municipio->nombres_municipio}}
-					@endif
-					</td>
-					<td align="center">
-					@if($u->entrega_municipal == 1)
-						{{$u->fecha_entrega_m}}
-					@endif
-					</td>
-					<td align="center"> 
-						@if($u->entrega_municipal == 0)
-							Vigente
-						@else
-							Usado
-						@endif
-					</td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-		{{Form::submit('Guardar', ['class'=>'btn btn-success'])}}
-		{{Form::close()}}
 	</div>
 
 	@stop
