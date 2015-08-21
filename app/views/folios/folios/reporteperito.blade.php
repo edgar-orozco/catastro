325e586d6468d68d25839a2187705d8aca100c15
Geo Distor <geodistor@MacBook-Pro-de-Geo.local>
2015-08-21 03:09:50 -0500

    Geo css Kiosko changes

--- app/views/folios/folios/reporteperito.blade.php
+++ app/views/folios/folios/reporteperito.blade.php
@@ -28,8 +28,9 @@
 	<div class="panel-heading">
 
 		<h4 class="panel-title">Reporte de Folios<p align="right"></h4>
-		Reporte Peritos Saldo<a href="formatoreporteperito" target="_blank" class="btn btn-xs btn-warning" title="Reimprimir"><i class="glyphicon glyphicon-print"></i></a>
-		Reporte Folios - Perito<a href="formatoreporteperito2" target="_blank" class="btn btn-xs btn-warning" title="Reimprimir"><i class="glyphicon glyphicon-print"></i></a></p>
+		Reporte Peritos Saldo &nbsp;<a href="formatoreporteperito" target="_blank" class="btn btn-xs btn-warning" title="Reimprimir"><i class="glyphicon glyphicon-print"></i></a>
+        <span> &nbsp;&nbsp;|&nbsp;&nbsp; </span>
+		Reporte Folios - Perito &nbsp;<a href="formatoreporteperito2" target="_blank" class="btn btn-xs btn-warning" title="Reimprimir"><i class="glyphicon glyphicon-print"></i></a></p>
 
 	</div>
 
