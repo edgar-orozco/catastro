1cd64eadce5184fe0b94f3eaa41bc001e018a889
Geo Distor <geodistor@MacBook-Pro-de-Geo.local>
2015-08-25 02:42:18 -0500

    Geo styles changes

--- app/views/Corevat/Avaluos/_inmueble.blade.php
+++ app/views/Corevat/Avaluos/_inmueble.blade.php
@@ -32,7 +32,7 @@
 	<div class="col-md-10 col-sm-10 col-xs-10"><h4>Detalles de Medidas y Colindancias</h4></div>
 
     <div class="col-md-2 col-sm-2 col-xs-2 btn-beside-title">
-        <a class="btn nuevo" id="btnNew" title="Nuevo Avaluo">
+        <a class="btn btn-primary nuevo" id="btnNew" title="Nuevo Avaluo">
             <span class="glyphicon glyphicon-plus-sign"></span> Nuevo
         </a>
     </div>
