bfa54ac578770307ecd92cee8960d6e3c7482d75
DeltaCastro <abel.castro9111@gmail.com>
2015-11-04 18:38:36 -0600

    Se corrige nombre del controlador

--- app/routes/ResultadoInspeccionRoutes.php
+++ app/routes/ResultadoInspeccionRoutes.php
@@ -2,7 +2,7 @@
 
 Route::group(array('before' => 'admin'), function () {
 
-	    Route::resource('tramites/resultadoInspeccion', 'tramites_resultadoInspeccionController');
+	    Route::resource('tramites/resultadoInspeccion', 'tramites_ResultadoInspeccionController');
     
 
 });
