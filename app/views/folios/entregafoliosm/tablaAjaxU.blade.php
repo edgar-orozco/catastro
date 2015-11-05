2ed43b9ccce3c2a1a64e49c9911f90abf7e90a01
DeltaCastro <abel.castro9111@gmail.com>
2015-11-05 14:46:47 -0600

    Se agrega vistas para la funcionalidad de entrega municipal

---
+++ app/views/folios/entregafoliosm/tablaAjaxU.blade.php
@@ -0,0 +1,48 @@
+		<table class="table datatable" id="rustico-table">
+			<thead>
+				<tr>
+					
+					<!--<th>{{Form::checkbox('', '', '', ['id'=>'todos'])}}</th>-->
+					<th>Marcar Folio Presentado</th>
+					<th>Folio Autorizados</th>
+					<th>Folio usado en el Municipio de:</th>
+					<th>Fecha de Recepción</th>
+					<th>Estado del Folio</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($fu as $u)
+					<tr>
+						<td align="center"> 
+							@if($u->entrega_municipal == 0)
+								{{Form::checkbox('urbanos[]', $u->numero_folio, '', ['class'=>'checkbox'])}}
+							@else
+								<i class="glyphicon glyphicon-ok"></i>
+							@endif
+						</td>
+						<td align="center"> {{$u->corevat()}}</td>
+						<td align="center"> 	
+							@if($u->entrega_municipal == 1)
+								{{$u->municipio->nombre_municipio}}
+							@endif	
+						<td align="center">
+							@if($u->entrega_municipal == 1  && $u->fecha_entrega_m)
+								{{$u->fecha_entrega_m}}
+							@endif
+						</td>
+						<td align="center"> 
+							@if($u->entrega_municipal == 0)
+								Vigente
+							@else
+								Usado
+							@endif
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+		{{$fu->links()}}
+        <div class="row">
+            <div class="col-md-6 col-sm-6">
+            </div>
+        </div>
--- app/views/folios/entregafoliosm/urbanos.blade.php
+++ app/views/folios/entregafoliosm/urbanos.blade.php
@@ -3,6 +3,22 @@
     {{ HTML::style('/css/bootstrap.min.css') }}
     {{ HTML::style('/css/dataTables.bootstrap.css') }}
 
+ @section('styles')
+    .spinner 
+    {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        margin-left: -50px; /* half width of the spinner gif */
+        margin-top: -50px; /* half height of the spinner gif */
+        text-align:center;
+        z-index:1234;
+        overflow: auto;
+        width: 100px; /* width of the spinner gif */
+        height: 102px; /*hight of the spinner gif +2px to fix IE8 issue */
+    }
+@stop
+
 @section('content')
 
 
@@ -15,76 +31,41 @@
     </div>
 
     <div class="panel-body">
-        {{Form::open(['id' => 'foliosMU', 'method' => 'GET'])}}
+        {{Form::open(['id' => 'foliosMR', 'method' => 'GET'])}}
         {{Form::select('year', $selectYear, null,  ['id' => 'year', 'class' => 'form-control input-sm', 'aria-controls' => 'emitidos-table'])}}
+        {{Form::select('estadoF', ['x'=>'-- Seleccione estado de Folio --', '0'=>'Vigente', '1'=>'Usado'], 'x',  ['id' => 'estadoF', 'class' => 'form-control input-sm', 'aria-controls' => 'emitidos-table'])}}
         {{Form::close()}}
-        <div class="row">
-            <div class="col-md-3">
-                <label>COREVAT</label>
-                <h5>{{$perito->corevat}}</h5>
+            <div class="row">
+                <div class="col-md-3">
+                    <label>COREVAT</label>
+                    <h5>{{$perito->corevat}}</h5>
+                </div>
+                <div class="col-md-3">
+                    <label>NOMBRE</label>
+                    <h5>{{$perito->nombre}}</h5>                
+                </div>
             </div>
-            <div class="col-md-3">
-                <label>NOMBRE</label>
-                <h5>{{$perito->nombre}}</h5>
+            <div class="row">
+            <div class="col-md-2">
+                    <label>Buscar {{Form::Text('Busqueda', null, ['id'=>'searchField', 'class' => 'form-control input-sm', 'aria-controls' => 'emitidos-table'])}}</label>
+                                    
             </div>
-        </div>
-        {{Form::open()}}
-        <table class="table datatable" id="example">
-            <thead>
-            <tr>
-                <!--<th>{{Form::checkbox('', '', '', ['id'=>'todos'])}}</th>-->
-                <th>Marcar Folio Presentado</th>
-                <th>Folio Autorizados</th>
-                <th>Folio usado en el Municipio de:</th>
-                <th>Fecha de Recepción</th>
-                <th>Estado del Folio</th>
-
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($fu as $u)
-
-
-                <tr>
-                    <td align="center">
-                        @if($u->entrega_municipal == 0)
-                            {{Form::checkbox('urbanos[]', $u->numero_folio, '', ['class'=>'checkbox'])}}
-                        @else
-                            <i class="glyphicon glyphicon-ok"></i>
-                        @endif
-                    </td>
-                    <?php
-                    $input = $u->numero_folio;
-                    $input = str_pad($input, 4, "0", STR_PAD_LEFT);
-                    ?>
-                    <td align="center"> {{$perito->corevat."-".$input.$u->tipo_folio."-15"}}</td>
-                    <td align="center">
-                        @if($u->entrega_municipal == 1)
-                            {{$u->municipio->nombres_municipio}}
-                        @endif
-                    </td>
-                    <td align="center">
-                        @if($u->entrega_municipal == 1)
-                            {{$u->fecha_entrega_m}}
-                        @endif
-                    </td>
-                    <td align="center">
-                        @if($u->entrega_municipal == 0)
-                            Vigente
-                        @else
-                            Usado
-                        @endif
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-        <div class="row">
-            <div class="col-md-6">
-                {{Form::submit('Guardar', ['class'=>'btn btn-block btn-success'])}}
-                {{Form::close()}}
+                
             </div>
-        </div>
+            {{Form::open(['id' => 'formFolios'])}}
+            <div id="tablaAjax">
+                @include('folios.entregafoliosm.tablaAjaxU')
+            </div>
+            <div id ="ajaxloading" class="spinner" style="display:none;">
+                <img id="img-spinner" src="/css/images/folios/spinner.gif" alt="Loading" width="70%" height="70%" />
+                Cargando...
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    {{Form::submit('Guardar', ['class'=>'btn btn-block btn-success', 'id'=>'guardarFolio' ])}}
+                    {{Form::close()}}
+                </div>
+            </div>
     </div>
 </div>
 
@@ -96,7 +77,8 @@
 	{{ HTML::script('/js/jquery/dataTables.bootstrap.js') }}
 
 	<script type="text/javascript">
-	$(document).ready(function(){
+	$(document).ready(function()
+    {
 
 		$('#year').on('change', function()
 			{
@@ -145,6 +127,82 @@
 
 
 	});
+    var $loading = $('#ajaxloading').hide();
+    $(document)
+      .ajaxStart(function () {
+        $loading.show();
+      })
+      .ajaxStop(function () {
+        $loading.hide();
+      });
+
+
+    $('#searchField').bind('input keyup', function()
+    {
+        var $this = $(this);
+        var delay = 1500; // 2 seconds delay after last input
+
+        clearTimeout($this.data('timer'));
+        $this.data('timer', setTimeout(function()
+        {
+            $this.removeData('timer');
+            
+            $.get('/entregafoliosmunicipal/tablaAjax/buscar', { buscar: $('#searchField').val(), id: getPath(3), tipo_folio: 'U' }, function(data) 
+            {
+                $('#tablaAjax').html(data);
+            });
+
+        }, delay));
+    });
+
+    $('#guardarFolio').on('click', function(e)
+    {
+        e.preventDefault();
+        $.post($(location).attr('pathname'), $( "#formFolios" ).serialize() + "&tipo_folio=U", function(data) 
+                {
+                    $('#tablaAjax').html(data);
+                }); 
+            
+    });
+
+            
+
+    $(document).on('click', '.pagination a', function(e)
+    {
+        e.preventDefault();
+        count = 0;
+        page = $(this).attr('href').split('page=')[1];
+        id = $(location).attr('href');
+        id = id.split('/')[5].split('?')[0];
+
+        $('.checkbox').each(function() 
+        { //loop through each checkbox
+                if(this.checked)
+                {
+                    count = count +1;
+                }  //select all checkboxes with class "checkbox1"              
+        });
+
+        if (count>0)
+        {
+            alert("Hay " + count + " folios marcados, por favor guarde cambios antes de cambiar de pagina");
+            return false;
+        }
+
+        $.get('/entregafoliosmunicipal/tablaAjax/?page='+page+'&id='+ id + '&tipo_folio=U', function(data)
+        {
+            $('#tablaAjax').html(data);
+        });
+
+    });
+
+    function getPath(numArray)
+    {
+        href =$(location).attr('pathname');
+        href = href.split('/');
+        path = href[numArray];
+        return path;
+    }
 
 	</script>
 
