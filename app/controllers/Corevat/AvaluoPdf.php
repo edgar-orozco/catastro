3b735f5f736702a2fd9b7d2ab6b73892d4ebd05f
carlosAgustin <carlos.agustin@ticase.com.mx>
2015-11-14 20:02:52 +0000

    COREVAT: Reporte de avalúo.

--- app/controllers/Corevat/AvaluoPdf.php
+++ app/controllers/Corevat/AvaluoPdf.php
@@ -2,13 +2,19 @@
 
 class AvaluoPdf extends \Anouar\Fpdf\Fpdf {
 
-	public function __construct($orientation = 'P', $unit = 'mm', $size = 'A4') {
+	public $logo_perito;
+
+	public function __construct($orientation = 'P', $unit = 'mm', $size = 'Letter') {
 		parent::__construct($orientation, $unit, $size);
 		$this->SetFillColor(164, 164, 164);
+		$this->logo_perito = public_path() . "/css/images/corevat/user-big-blank.jpg";
 	}
+
 	function Header() {
-		$this->Image(public_path() . "/css/images/corevat/crv-01.jpg", 5, 5, 139.57, 26.20);
-		$this->Ln(25);
+		//global $logo_perito;
+		$this->Image(public_path() . "/css/images/corevat/crv-01.jpg", null, null, 130, 25);
+		$this->Image($this->logo_perito, 170, 10, 30, 25);
+		$this->Ln(5);
 	}
 
 	function Footer() {
@@ -16,4 +22,111 @@ class AvaluoPdf extends \Anouar\Fpdf\Fpdf {
 		$this->SetFont('Arial', 'I', 8);
 		$this->Cell(0, 10, utf8_decode('Página ') . $this->PageNo() . '/{nb}', 0, 0, 'C');
 	}
+
+	function MultiCell($w, $h, $txt, $border = 0, $align = 'J', $fill = false, $maxline = 0) {
+		//Output text with automatic or explicit line breaks, at most $maxline lines
+		$cw = &$this->CurrentFont['cw'];
+		if ($w == 0)
+			$w = $this->w - $this->rMargin - $this->x;
+		$wmax = ($w - 2 * $this->cMargin) * 1000 / $this->FontSize;
+		$s = str_replace("\r", '', $txt);
+		$nb = strlen($s);
+		if ($nb > 0 && $s[$nb - 1] == "\n")
+			$nb--;
+		$b = 0;
+		if ($border) {
+			if ($border == 1) {
+				$border = 'LTRB';
+				$b = 'LRT';
+				$b2 = 'LR';
+			} else {
+				$b2 = '';
+				if (is_int(strpos($border, 'L')))
+					$b2.='L';
+				if (is_int(strpos($border, 'R')))
+					$b2.='R';
+				$b = is_int(strpos($border, 'T')) ? $b2 . 'T' : $b2;
+			}
+		}
+		$sep = -1;
+		$i = 0;
+		$j = 0;
+		$l = 0;
+		$ns = 0;
+		$nl = 1;
+		while ($i < $nb) {
+			//Get next character
+			$c = $s[$i];
+			if ($c == "\n") {
+				//Explicit line break
+				if ($this->ws > 0) {
+					$this->ws = 0;
+					$this->_out('0 Tw');
+				}
+				$this->Cell($w, $h, substr($s, $j, $i - $j), $b, 2, $align, $fill);
+				$i++;
+				$sep = -1;
+				$j = $i;
+				$l = 0;
+				$ns = 0;
+				$nl++;
+				if ($border && $nl == 2)
+					$b = $b2;
+				if ($maxline && $nl > $maxline)
+					return substr($s, $i);
+				continue;
+			}
+			if ($c == ' ') {
+				$sep = $i;
+				$ls = $l;
+				$ns++;
+			}
+			$l+=$cw[$c];
+			if ($l > $wmax) {
+				//Automatic line break
+				if ($sep == -1) {
+					if ($i == $j)
+						$i++;
+					if ($this->ws > 0) {
+						$this->ws = 0;
+						$this->_out('0 Tw');
+					}
+					$this->Cell($w, $h, substr($s, $j, $i - $j), $b, 2, $align, $fill);
+				} else {
+					if ($align == 'J') {
+						$this->ws = ($ns > 1) ? ($wmax - $ls) / 1000 * $this->FontSize / ($ns - 1) : 0;
+						$this->_out(sprintf('%.3F Tw', $this->ws * $this->k));
+					}
+					$this->Cell($w, $h, substr($s, $j, $sep - $j), $b, 2, $align, $fill);
+					$i = $sep + 1;
+				}
+				$sep = -1;
+				$j = $i;
+				$l = 0;
+				$ns = 0;
+				$nl++;
+				if ($border && $nl == 2)
+					$b = $b2;
+				if ($maxline && $nl > $maxline) {
+					if ($this->ws > 0) {
+						$this->ws = 0;
+						$this->_out('0 Tw');
+					}
+					return substr($s, $i);
+				}
+			} else
+				$i++;
+		}
+		//Last chunk
+		if ($this->ws > 0) {
+			$this->ws = 0;
+			$this->_out('0 Tw');
+		}
+		if ($border && is_int(strpos($border, 'B')))
+			$b.='B';
+		$this->Cell($w, $h, substr($s, $j, $i - $j), $b, 2, $align, $fill);
+		$this->x = $this->lMargin;
+		return '';
+	}
+
 }
