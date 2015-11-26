f1eccfdc92cf1bd0c707c04954335fc056e869f1
vagrant <rmcentinela@gmail.com>
2015-11-26 14:57:45 -0600

    se carga seccion actualizacion v1.3 de macros copropietario para solicitud de inspeccion

--- public/js/macros/copropietario.js
+++ public/js/macros/copropietario.js
@@ -79,6 +79,65 @@ $(document).on('click','.agregarPersonaFisica',function () {
 
 
 
+$(document).on('click','.agregarPersonaMoral',function () {
+
+    var $div = $('div[id^="personaDiv"]:last');
+    var num = parseInt($div.prop("id").match(/\d+/g), 10) + 1;
+
+
+    var clon = $div.clone(true, true).prop('id', 'personaDiv' + num);
+
+    var curp = clon.find('input')[0];
+
+    var rfc = clon.find('input')[1];
+    var rfc2 = clon.find('input')[1];
+
+    var nombres = clon.find('input')[2];
+    var nombres2 = clon.find('input')[2];
+
+    var apellido_paterno = clon.find('input')[3];
+    var apellido_materno = clon.find('input')[4];
+
+    var label = clon.find('label')[0];
+    var label2 = clon.find('label')[1];
+    var label3 = clon.find('label')[2];
+    var label4 = clon.find('label')[3];
+    var label5 = clon.find('label')[4];
+
+    //modificamos el nombre
+
+    //curp.name = curp.name.replace(/(\w+)\[(\d+)\]\[(\w+)\]/, "$1[" + num + "][$3]");
+     rfc.name = rfc.name.replace(/(\w+)\[(\d+)\]\[(\w+)\]/, "$1[" + num + "][$3]");
+     rfc2.id = rfc2.id.replace(/(\w+)\[(\d+)\]\[(\w+)\]/, "$1[" + num + "][$3]");
+
+      nombres.name = nombres.name.replace(/(\w+)\[(\d+)\]\[(\w+)\]/, "$1[" + num + "][$3]");
+      nombres2.id = nombres2.id.replace(/(\w+)\[(\d+)\]\[(\w+)\]/, "$1[" + num + "][$3]");
+
+       //apellido_paterno.name = apellido_paterno.name.replace(/(\w+)\[(\d+)\]\[(\w+)\]/, "$1[" + num + "][$3]");
+        //apellido_materno.name = apellido_materno.name.replace(/(\w+)\[(\d+)\]\[(\w+)\]/, "$1[" + num + "][$3]");
+
+
+    //los creamos en blanco
+    $(curp).hide();
+    $(rfc).val('');
+    $(nombres).val('');
+    $(apellido_paterno).hide();
+    $(apellido_materno).hide();
+
+    $(label).hide();
+    $(label4).hide();
+    $(label5).hide();
+
+
+    $($div).after(clon);
+
+});
+
+
+
+
+
+
 $(document).on('click','.quitarPersona',function () {
     var confirma = confirm('¿Está seguro que desea borrar la Persona?');
     if (confirma == true) {
