3ded94e02b54e045b03d57a673b0a459327921cf
Daniel Rivera Reyes <dani.river91@gmail.com>
2015-11-26 16:17:05 -0600

    se integra el pdf de memos

--- app/routes/MemosRoutes.php
+++ app/routes/MemosRoutes.php
@@ -3,3 +3,6 @@
 Route::get('reportes/memos.{format}',
         array('as'=>'IndexMemos', 'uses' => 'memos_MemosController@index'));
 Route::resource('reportes/memos', 'memos_MemosController');
+
+//Ruta para el recibo en pdf
+Route::get("memos/recibo", "memos_MemosController@get_pdf");
---
+++ app/views/reportes/memos/memo.blade.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MEMO</title>
+	</head>
+	<style>
+		#contenedor{  
+        //background-color:#F4ABF2;
+        background-image: url("css/images/main/logo-header.png-agua.png");
+        background-repeat: no-repeat;
+        background-position: center;
+        //border:2px solid #000000; 
+        font-family: Verdana, Arial, Helvetica, sans-serif;
+        font-size: 12px;
+        color: #0000000;
+        width: 100%;
+        height: 97%;
+		}  
+		#cabecera{  
+		        //background-color:#E5BC7A;  
+		        //height:20%;  
+		}  
+		#menu{  
+		        height:10%;  
+		        background-color:#C8CACC;  
+		}  
+		#centro{  
+		        //height:25%;  
+		        //background-color:#BDD2EF; 
+		        border: 2px solid #000000;
+		        margin: 5px;
+		        width:100%;
+		}
+		#centro2{  
+		        height:10%;  
+		        //background-color:#BDD2EF; 
+		        //border: 2px solid #000000;
+		        margin: 5px;
+		        width:100%;
+		}
+		#centro3{
+		        height:4%;  
+		        //background-color:#BDD2EF; 
+		        //border: 2px solid #000000;
+		        margin: 5px;
+		        width:100%;
+		}
+		#dentro{
+		    height: 4%;
+		    //background-color: #FFFF99;
+		    margin: 5px;
+		    width: 100%;
+		    /*tipo de letra*/
+		    font-family: 'Arial Black', Gadget, sans-serif;
+		    text-align: center;
+		    font-size: 16px;
+		    color: #0000000; 
+		}
+		#izquierda{  
+		        height:26%;
+		        //background-color:blanchedalmond;
+		        display:inline-block;
+		        float:left;  
+		        width:50%; 
+		        margin: 2px;
+		}  
+		#derecha{  
+		        height:26%;  
+		        //background-color:burlywood;
+		        display:inline-block;
+		        float:right;  
+		        width:48.3%;
+		        margin: 2px;
+		}  
+
+		#pie{  
+		        height:10%;  
+		        //background-color:#D3D1C1;  
+		        clear:both;
+		        margin: 4px;
+		}
+		.title{
+		        font-family: 'Arial Black', Gadget, sans-serif;
+		        text-align: center;
+		        font-size: 13px;
+		        color: #0000000; 
+		}
+	</style>
+	<body>
+		<div id="contenedor">
+			<div id="cabecera">
+				<table width="100%">
+		            <tr>
+		                <td width="20%" align="right"><img src="css/images/main/main-logo.png"  height="70"></td>
+		                <td width="60%" align="center" class="title">H. AYUNTAMIENTO CONSTITUCIONAL DE HUIMANGUILLO, TAB.<br>DIRECCION DE FINANZAS<br>SUBDIRECCIÓN DE CATASTRO</td>
+		                <td width="20%" align="center"><img src="css/images/home/logos/LogoHuimanguillo.png"  height="100"></td>
+		            </tr>
+		        </table>
+		       	<table width="100%">
+		            <tr>
+		                <td width="33.33%">Lugar: , Tabasco.</td>
+		                <td width="33.33%" align="center"><strong>NOTIFICACION CATASTRAL</strong></td>
+		                <td width="33.33%" align="right">Fecha de Impresión:</td>
+		            </tr>
+		            <tr>
+		                <td width="33.33%">Fecha de Valuación:</td>
+		                <td width="33.33%" ></td>
+		                <td width="33.33%" align="right"></td>
+		            </tr>
+		        </table>
+		        LIC.<br>
+		        Director de Catastro de la SEFIN<br>
+		        Presente:
+		        <p>Por este conducto notifico a usted de la gestion catastral practicada en esta oficina referente al tramite denminado:<br>
+		        <u>REGISTRO DE ESCRITURA PUBLICA</u>. Afectando a la(s) cuenta(s) siguente(s):</p>
+			</div>
+			<div id="centro">
+				<div id="dentro"><strong>ANTECEDENTES</strong></div>
+				<table width="100%">
+					<tr>
+						<td width="33.33%">TIPO DE PREDIO:</td>
+						<td width="33.33%" align="center">CUENTA:</td>
+						<td width="33.33%" align="right">CLAVE:</td>
+					</tr>
+				</table>
+				<div id="centro">
+					<table width="100%">
+						<tr>
+							<td width="33.33%">PROPIETARIO:</td>
+							<td width="33.33%">RFC:</td>
+							<td width="33.33%">TIPO PROPIETARIO:</td>
+						</tr>
+					</table>
+					<u>Domicilio Particular:</u><br><br>
+					<table width="100%">
+						<tr>
+							<td width="33.33%">FRACCIONAMIENTO/COLONIA</td>
+							<td width="33.33%">COD. POSTAL</td>
+							<td width="33.33%">POBLACION</td>
+						</tr>
+					</table>
+				</div>
+				<div id="centro">
+					<u>UBICACION DEL PREDIO</u><br><br>
+					<table width="100%">
+						<tr>
+							<td width="33.33%">FRACCIONAMIENTO/COLONIA</td>
+							<td width="33.33%">COD. POSTAL</td>
+							<td width="33.33%">POBLACION</td>
+						</tr>
+					</table>
+					USO DEL PREDIO:
+				</div>
+				<div id="centro">
+					<table width="100%">
+						<tr>
+							<td width="33.33%">SUP. PREDIO:</td>
+							<td width="33.33%">AREA CONSTRUCCION:</td>
+							<td width="33.33%">NIVELES:</td>
+						</tr>
+						<tr>
+							<td width="33.33%">TIPO DE CONSTRUCCION:</td>
+							<td width="33.33%"></td>
+							<td width="33.33%">NIVELES:</td>
+						</tr>
+						<tr>
+							<td width="33.33%">VALOR DE LA CONSTRUCCION:</td>
+							<td width="33.33%">VALOR DEL TERRENO:</td>
+							<td width="33.33%">VALOR CATASTRAL:</td>
+						</tr>
+						<tr>
+							<td width="33.33%">ESCRITURA:</td>
+							<td width="33.33%">NUM. REG.|PREDIO|FOLIO:</td>
+							<td width="33.33%">VOL:   FECHA:</td>
+						</tr>
+						<tr>
+							<td width="33.33%">ESTADO FISCAL:</td>
+							<td width="33.33%"></td>
+							<td width="33.33%">FECHA DE MODIFICACION:</td>
+						</tr>
+					</table>
+					OBSERVACIONES:
+				</div>
+			</div>
+			<div id="centro">
+				<div id="dentro"><strong>CAMBIO</strong></div>
+				<table width="100%">
+					<tr>
+						<td width="33.33%">TIPO DE PREDIO:</td>
+						<td width="33.33%" align="center">CUENTA:</td>
+						<td width="33.33%" align="right">CLAVE:</td>
+					</tr>
+				</table>
+				<div id="centro">
+					<table width="100%">
+						<tr>
+							<td width="33.33%">PROPIETARIO:</td>
+							<td width="33.33%">RFC:</td>
+							<td width="33.33%">TIPO PROPIETARIO:</td>
+						</tr>
+					</table>
+					<u>Domicilio Particular:</u><br><br>
+					<table width="100%">
+						<tr>
+							<td width="33.33%">FRACCIONAMIENTO/COLONIA</td>
+							<td width="33.33%">COD. POSTAL</td>
+							<td width="33.33%">POBLACION</td>
+						</tr>
+					</table>
+				</div>
+				<div id="centro">
+					<u>UBICACION DEL PREDIO</u><br><br>
+					<table width="100%">
+						<tr>
+							<td width="33.33%">FRACCIONAMIENTO/COLONIA</td>
+							<td width="33.33%">COD. POSTAL</td>
+							<td width="33.33%">POBLACION</td>
+						</tr>
+					</table>
+					USO DEL PREDIO:
+				</div>
+				<div id="centro">
+					<table width="100%">
+						<tr>
+							<td width="33.33%">SUP. PREDIO:</td>
+							<td width="33.33%">AREA CONSTRUCCION:</td>
+							<td width="33.33%">NIVELES:</td>
+						</tr>
+						<tr>
+							<td width="33.33%">TIPO DE CONSTRUCCION:</td>
+							<td width="33.33%"></td>
+							<td width="33.33%">NIVELES:</td>
+						</tr>
+						<tr>
+							<td width="33.33%">VALOR DE LA CONSTRUCCION:</td>
+							<td width="33.33%">VALOR DEL TERRENO:</td>
+							<td width="33.33%">VALOR CATASTRAL:</td>
+						</tr>
+						<tr>
+							<td width="33.33%">ESCRITURA:</td>
+							<td width="33.33%">NUM. REG.|PREDIO|FOLIO:</td>
+							<td width="33.33%">VOL:   FECHA:</td>
+						</tr>
+						<tr>
+							<td width="33.33%">ESTADO FISCAL:</td>
+							<td width="33.33%"></td>
+							<td width="33.33%">FECHA DE MODIFICACION:</td>
+						</tr>
+					</table>
+					OBSERVACIONES:
+				</div>
+			</div>
+			<br><br><br><br><br><br><br><br>
+			<div id="centro">
+				<div id="dentro"><strong>ALTA</strong></div>
+				<table width="100%">
+					<tr>
+						<td width="33.33%">TIPO DE PREDIO:</td>
+						<td width="33.33%" align="center">CUENTA:</td>
+						<td width="33.33%" align="right">CLAVE:</td>
+					</tr>
+				</table>
+				<div id="centro">
+					<table width="100%">
+						<tr>
+							<td width="33.33%">PROPIETARIO:</td>
+							<td width="33.33%">RFC:</td>
+							<td width="33.33%">TIPO PROPIETARIO:</td>
+						</tr>
+					</table>
+					<u>Domicilio Particular:</u><br><br>
+					<table width="100%">
+						<tr>
+							<td width="33.33%">FRACCIONAMIENTO/COLONIA</td>
+							<td width="33.33%">COD. POSTAL</td>
+							<td width="33.33%">POBLACION</td>
+						</tr>
+					</table>
+				</div>
+				<div id="centro">
+					<u>UBICACION DEL PREDIO</u><br><br>
+					<table width="100%">
+						<tr>
+							<td width="33.33%">FRACCIONAMIENTO/COLONIA</td>
+							<td width="33.33%">COD. POSTAL</td>
+							<td width="33.33%">POBLACION</td>
+						</tr>
+					</table>
+					USO DEL PREDIO:
+				</div>
+				<div id="centro">
+					<table width="100%">
+						<tr>
+							<td width="33.33%">SUP. PREDIO:</td>
+							<td width="33.33%">AREA CONSTRUCCION:</td>
+							<td width="33.33%">NIVELES:</td>
+						</tr>
+						<tr>
+							<td width="33.33%">TIPO DE CONSTRUCCION:</td>
+							<td width="33.33%"></td>
+							<td width="33.33%">NIVELES:</td>
+						</tr>
+						<tr>
+							<td width="33.33%">VALOR DE LA CONSTRUCCION:</td>
+							<td width="33.33%">VALOR DEL TERRENO:</td>
+							<td width="33.33%">VALOR CATASTRAL:</td>
+						</tr>
+						<tr>
+							<td width="33.33%">ESCRITURA:</td>
+							<td width="33.33%">NUM. REG.|PREDIO|FOLIO:</td>
+							<td width="33.33%">VOL:   FECHA:</td>
+						</tr>
+						<tr>
+							<td width="33.33%">ESTADO FISCAL:</td>
+							<td width="33.33%"></td>
+							<td width="33.33%">FECHA DE MODIFICACION:</td>
+						</tr>
+					</table>
+					OBSERVACIONES:
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
