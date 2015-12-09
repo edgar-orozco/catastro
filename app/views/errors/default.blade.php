7a4adc837c2eb5f5e14c298369ec21ac441920cc
edgar-orozco <edgar.orozco@bxd.mx>
2015-12-09 01:56:28 -0600

    Update default.blade.php
    Se modifica mensaje de error para reporte de usuario.

--- app/views/errors/default.blade.php
+++ app/views/errors/default.blade.php
@@ -27,12 +27,14 @@
                 <div class="panel-body">
                     <a href="javascript:window.history.back();" class="btn btn-primary pull-right" role="button"><i class="glyphicon glyphicon-arrow-left"></i> Regresar</a>
                     <br>
-                    <div align="center">
+                    <div align="center" style="border:dashed thin silver;">
                         <h1>Error {{$code}}</h1>
                         <h2>Se ha presentado un error no definido.</h2>
                         <h4>{{date("Y-m-d H:i:s")}}</h4>
                         <h4>{{Request::getClientIp()}}</h4>
-                        <h5>Favor de reportar esta pantalla de error. Si no le es posible realizar una captura de pantalla copie el código de error y la fecha y hora que aparecen en el mensaje anterior</h5>
+                    </div>
+                    <div align="center">
+                        <h4>Favor de reportar esta pantalla de error. Si no le es posible realizar una captura de pantalla copie el código de error y la fecha y hora que aparecen en el mensaje anterior</h4>                        
                     </div>
                 </div>
             </div>
