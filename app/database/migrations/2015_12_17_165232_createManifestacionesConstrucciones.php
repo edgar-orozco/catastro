12bf3be45972c9252d047896d4c2a34c33c54b3a
deltacastro <abel.castro9111@gmail.com>
2015-12-17 17:06:03 -0600

    Se modifica clase de la migracion

--- app/database/migrations/2015_12_17_165232_createManifestacionesConstrucciones.php
+++ app/database/migrations/2015_12_17_165232_createManifestacionesConstrucciones.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ManiConstruccionesForeignKeys extends Migration {
+class CreateManifestacionesConstrucciones extends Migration {
 
 	/**
 	 * Run the migrations.
