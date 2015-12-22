932022f930a53cb3e51770a23ab9924c1de5596c
deltacastro <abel.castro9111@gmail.com>
2015-12-21 18:51:54 -0600

    Se agrega el cargado de imagenes

--- app/views/tramites/inspeccion/_form_load_images.blade.php
+++ app/views/tramites/inspeccion/_form_load_images.blade.php
@@ -3,7 +3,7 @@
 
     <label class="control-label">Select File</label>
 
-    <input id="input-700" name="kartik-input-700[]" type="file" multiple class="file-loading" data-show-preview="false">
+    <input id="input-1" name="imagen" type="file"  data-show-preview="true" multiple data-show-upload="false" data-show-caption="true">
 </div>
 <br>
 {{Form::label('imagen','Imagenes cargadas')}}
@@ -11,46 +11,8 @@
 
 
 <div class="row">
-    <div class="row col-md-8">
-        <div style="font-size:15px" class="col-md-4">
-            imagen 1
-        </div>
-        <div class="col-md-4">
-            <button class="btn btn-warning " title="Eliminar Imagen"><i class="glyphicon glyphicon-trash"></i></button>
-            <button class="btn btn-info" title="Descargar Imagen"><i class="glyphicon glyphicon-download-alt"></i></button>
-        </div>
+    <div class="col-md-4-offset-0">
+        <img src="{{$consultaMani->fachada}}" width="300" height="300">
     </div>
-    <div class="row col-md-8">
-        <div style="font-size:15px" class="col-md-4">
-            imagen 1
-        </div>
-        <div class="col-md-4">
-            <button class="btn btn-warning " title="Eliminar Imagen"><i class="glyphicon glyphicon-trash"></i></button>
-            <button class="btn btn-info" title="Descargar Imagen"><i class="glyphicon glyphicon-download-alt"></i></button>
-        </div>
-    </div>
-    <div style="font-size:15px" class="row col-md-8">
-        <div class="col-md-4">
-            imagen 1
-        </div>
-        <div class="col-md-4">
-            <button class="btn btn-warning " title="Eliminar Imagen"><i class="glyphicon glyphicon-trash"></i></button>
-            <button class="btn btn-info" title="Descargar Imagen"><i class="glyphicon glyphicon-download-alt"></i></button>
-        </div>
-    </div>
-    <div class="row col-md-8">
-        <div style="font-size:15px" class="col-md-4">
-            imagen 1
-        </div>
-        <div class="col-md-4">
-            <button class="btn btn-warning " title="Eliminar Imagen"><i class="glyphicon glyphicon-trash"></i></button>
-            <button class="btn btn-info" title="Descargar Imagen"><i class="glyphicon glyphicon-download-alt"></i></button>
-        </div>
-    </div>
-
-        <div class="col-md-4-offset-0">
-        <img src="/complementarios/anexos/27/008/002-0007-000008/Centla.jpg.jpg" id="show-image">
-    </div>
-
 </div>
 
