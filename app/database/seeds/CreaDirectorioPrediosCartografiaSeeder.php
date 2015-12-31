6ab0a869b20ddf48ea88ee6cebf3f15458632883
roman08 <rmcentinela@gmail.com>
2015-12-30 19:11:30 -0600

    Se crea directorio para guardar archivos generados en la consulta cartografica de predios v.3

--- app/database/seeds/CreaDirectorioPrediosCartografiaSeeder.php
+++ app/database/seeds/CreaDirectorioPrediosCartografiaSeeder.php
@@ -5,7 +5,7 @@ class CreaDirectorioPrediosCartografiaSeeder extends Seeder {
 
       
 
-        $carpeta = '/ResultadoCartografia/predios';
+        $carpeta = '/complementarios/predios';
 if (!file_exists(public_path() . $carpeta)) {
     mkdir(public_path() .$carpeta, 0777, true);
 }
