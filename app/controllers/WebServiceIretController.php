0317eb882c052fe9d4b2fde9e452516df7cbc37b
roman08 <rmcentinela@gmail.com>
2016-01-19 12:55:27 -0600

    se carga validacion para campo de folio volantes en el modulo consulta de volante IRET

--- app/controllers/WebServiceIretController.php
+++ app/controllers/WebServiceIretController.php
@@ -36,7 +36,31 @@ class WebServiceIretController extends \BaseController {
 	 */
 	public function store()
 	{
-		$volante=Input::get('volante');
+
+		     $volante=Input::get('volante');
+    $predios = sprintf("http://www.irettabasco.gob.mx:8080/volante/%s", $volante);
+    try{
+        $content = json_decode(file_get_contents($predios));
+        $res = json_last_error();
+        if($res == JSON_ERROR_NONE){
+          //Al parecer todo está bien.
+          return View::make('WebServiceIret.TablaWebService', compact('content'));
+        }else{
+          //Aquí el servidor aunque no contestó con un error 500 no nos mandó un objeto json.
+          //Habría que responder con un mensaje al usuario, talvez otra plantilla diferente a WebServiceIret.TablaWebService ?
+            echo '<div class="alert alert-danger">No existe registro para el numero de folio ingresado.</div>';
+            exit;
+        }
+    }
+    catch(Exception $e)
+    {
+      //Aquí deplano el servidor se indigestó y mando un error 500 o algun otro codigo de error
+      //Habria tambien que indicarle al usuario de forma amable que lo que metió no arrojó ningun dato 
+      //y por ningun motivo mostrarle el error de oracle que sale si se le mete una letra en lugar de un numero
+        echo '<div class="alert alert-danger">Erro de respuesta del servidor.</div>';
+        echo $e->getMessage();
+    }
+		/*$volante=Input::get('volante');
 		$predios = sprintf("http://www.irettabasco.gob.mx:8080/volante/%s", $volante);
 		try{
 		$content = json_decode(file_get_contents($predios));
@@ -49,7 +73,7 @@ class WebServiceIretController extends \BaseController {
 							'result' => $e,						
 							'succes' => 'succes'
 						));
-				}
+				}*/
 	}
 
 
--- app/views/WebServiceIret/index.blade.php
+++ app/views/WebServiceIret/index.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.default')
-
+@section('styles')
+.spinner {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    margin-left: -50px; /* half width of the spinner gif */
+    margin-top: -50px; /* half height of the spinner gif */
+    text-align:center;
+    z-index:1234;
+    overflow: auto;
+    width: 100px; /* width of the spinner gif */
+    height: 102px; /*hight of the spinner gif +2px to fix IE8 issue */
+}
+@stop
 @section('title')
   {{{ $title }}} :: @parent
 @stop
@@ -11,7 +24,7 @@
         {{ Form::open(array('id' => 'WSI')) }}
 <div class="col-md-6 form-group">
        {{Form::label('volante','Folio del Volante ')}}
-      {{Form::text('volante', null, ['class'=>'form-control','autofocus'=> 'autofocus', 'required' => 'required', 'maxlength' => 15, 'size' => 15] )}}
+      {{Form::text('volante', null, ['class'=>'form-control','autofocus'=> 'autofocus', 'required' => 'required', 'maxlength' => 15, 'size' => 10, 'placeholder' => '00000'] )}}
 </div>
         <div class="form-actions form-group col-md-6" style="clear:both; ">
             {{ Form::submit('Consultar', array('class' => 'btn btn-primary')) }}
@@ -23,7 +36,10 @@
     </div>
 
 <div id="tablaAjax"></div>
-  
+  <div id ="ajaxloading" class="spinner" style="display:none;">
+      <img id="img-spinner" src="/css/images/folios/spinner.gif" alt="Loading" width="70%" height="70%" />
+      Cargando...
+    </div>
 @stop
 @section('javascript')
 <script type="text/javascript">
@@ -40,6 +56,7 @@
                     success: function (data)
                     {
                       console.log(data);
+                      
                       if(data.succes == 'succes')
                       {
                                           $('#tablaAjax').html('<div class="alert alert-danger">No existe registro para el numero de folio ingresado.</div>');
@@ -55,6 +72,38 @@
                 });
         return false;
     });
+var $loading = $('#ajaxloading').hide();
+  $(document)
+    .ajaxStart(function () {
+      $loading.show();
+    })
+    .ajaxStop(function () {
+      $loading.hide();
+    });
 
+
+ $(document).ready(function () {
+        $("#volante").keydown(function (event) {
+            if (event.shiftKey)
+            {
+                event.preventDefault();
+            }
+
+            if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 189 || event.keyCode == 109) {
+            }
+            else {
+                if (event.keyCode < 95) {
+                    if (event.keyCode < 48 || event.keyCode > 57) {
+                        event.preventDefault();
+                    }
+                }
+                else {
+                    if (event.keyCode < 96 || event.keyCode > 105) {
+                        event.preventDefault();
+                    }
+                }
+            }
+        });
+    });
 </script>
 @stop
